dea4d80ebab6e990f0477d14b3521dd40f065118
Hao Dong <haodong@dyn1231-41.wlan.ic.ac.uk>
2016-06-15 16:19:56 +0100

    change theme

--- conf.py
+++ conf.py
@@ -21,6 +21,13 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+# lasagne use this theme https://github.com/snide/sphinx_rtd_theme
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -126,7 +133,9 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+# html_theme = "sphinx_rtd_theme"
+# html_theme_path = ["_themes", ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
