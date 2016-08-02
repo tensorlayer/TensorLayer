97ab129a7c2cbc5b2041bf1c6781ed2205c20686
angerhang <514140809@qq.com>
2016-08-02 16:50:54 +0100

    Fix missing python source code doc.

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,8 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os, sys
+sys.path.insert(0, os.path.abspath("../"))
 
 # -- General configuration ------------------------------------------------
 
