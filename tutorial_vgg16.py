2d98c52643a46501b913ab7aab24afe5a1abde00
zsdonghao <dhsig552@163.com>
2016-09-21 18:26:09 +0100

    add VGG 16 example, provide tool to convert the Caffe model to TensorLayer

--- tutorial_vgg16.py
+++ tutorial_vgg16.py
@@ -23,8 +23,27 @@ which is a dataset of over 14 million images belonging to 1000 classes.
 
 Download Pre-trained Model
 ----------------------------
-Model weights - vgg16_weights.npz http://www.cs.toronto.edu/~frossard/post/vgg16/
+Model weights in this example - vgg16_weights.npz : http://www.cs.toronto.edu/~frossard/post/vgg16/
 
+Caffe VGG 16 model : https://gist.github.com/ksimonyan/211839e770f7b538e2d8#file-readme-md
+
+Tool to convert the Caffe models to TensorFlow's : https://github.com/ethereon/caffe-tensorflow
+
+Note
+------
+When feeding other images to the model be sure to properly resize or crop them
+beforehand. Distorted images might end up being misclassified. One way of safely
+feeding images of multiple sizes is by doing center cropping, as shown in the
+following snippet:
+
+>>> image_h, image_w, _ = np.shape(img)
+>>> shorter_side = min(image_h, image_w)
+>>> scale = 224. / shorter_side
+>>> image_h, image_w = np.ceil([scale * image_h, scale * image_w]).astype('int32')
+>>> img = imresize(img, (image_h, image_w))
+>>> crop_x = (image_w - 224) / 2
+>>> crop_y = (image_h - 224) / 2
+>>> img = img[crop_y:crop_y+224,crop_x:crop_x+224,:]
 """
 
 
