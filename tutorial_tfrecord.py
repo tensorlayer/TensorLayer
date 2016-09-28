f7ffc418731f0573f854b80f03d398db94dd8600
zsdonghao <dhsig552@163.com>
2016-09-28 10:24:00 +0100

    Add TFRecord Reader Tutorial, effectively save data into TF format and  read data from TF format

---
+++ tutorial_tfrecord.py
@@ -0,0 +1,125 @@
+#! /usr/bin/python
+# -*- coding: utf8 -*-
+
+
+import tensorflow as tf
+import tensorlayer as tl
+import os
+from PIL import Image
+import io
+
+
+"""
+You will learn
+1. How to save data into TFRecord format.
+2. How to read data from TFRecord format by using Queue and Thread.
+
+Reference
+---------
+English : https://www.tensorflow.org/versions/master/api_docs/python/io_ops.html#readers
+Chinese : http://blog.csdn.net/u012759136/article/details/52232266
+          https://github.com/ycszen/tf_lab/blob/master/reading_data/TensorFlow高效加载数据的方法.md
+"""
+
+## Save data ==================================================================
+classes = ['/data/cat', '/data/dog']  # cat is 0, dog is 1
+cwd = os.getcwd()
+writer = tf.python_io.TFRecordWriter("train.tfrecords")
+for index, name in enumerate(classes):
+    class_path = cwd + name + "/"
+    for img_name in os.listdir(class_path):
+        img_path = class_path + img_name
+        img = Image.open(img_path)
+        img = img.resize((224, 224))
+        ## Visualize the image as follow:
+        # tl.visualize.frame(I=img, second=5, saveable=False, name='frame', fig_idx=12836)
+        ## Converts a image to bytes
+        img_raw = img.tobytes()
+        ## Convert the bytes back to image as follow:
+        # image = Image.frombytes('RGB', (224,224), img_raw)
+        # tl.visualize.frame(I=image, second=1, saveable=False, name='frame', fig_idx=1236)
+        ## Write the data into TF format
+        # image     : Feature + BytesList
+        # label     : Feature + Int64List or FloatList
+        # sentence  : FeatureList + Int64List , see Google's im2txt example
+        example = tf.train.Example(features=tf.train.Features(feature={
+            "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[index])),
+            'img_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw])),
+        }))
+        writer.write(example.SerializeToString())  # Serialize To String
+writer.close()
+
+
+## Load Data Method 1: Simple read ============================================
+# read data one by one in order
+for serialized_example in tf.python_io.tf_record_iterator("train.tfrecords"):
+    example = tf.train.Example()
+    example.ParseFromString(serialized_example)
+    img_raw = example.features.feature['img_raw'].bytes_list.value
+    label = example.features.feature['label'].int64_list.value
+    ## converts a image from bytes
+    image = Image.frombytes('RGB', (224, 224), img_raw[0])
+    tl.visualize.frame(image, second=1, saveable=False, name='frame', fig_idx=1283)
+    print(label)
+
+
+## Read Data Method 2: Queue and Thread =======================================
+# use sess.run to get a batch of data
+def read_and_decode(filename):
+    #根据文件名生成一个队列
+    filename_queue = tf.train.string_input_producer([filename])
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)   #返回文件名和文件
+    features = tf.parse_single_example(serialized_example,
+                                       features={
+                                           'label': tf.FixedLenFeature([], tf.int64),
+                                           'img_raw' : tf.FixedLenFeature([], tf.string),
+                                       })
+    # You can do more image distortion here for training data
+    img = tf.decode_raw(features['img_raw'], tf.uint8)
+    img = tf.reshape(img, [224, 224, 3])
+    # img = tf.cast(img, tf.float32) * (1. / 255) - 0.5
+    label = tf.cast(features['label'], tf.int32)
+    return img, label
+
+img, label = read_and_decode("train.tfrecords")
+
+## Use shuffle_batch or batch
+# see https://www.tensorflow.org/versions/master/api_docs/python/io_ops.html#shuffle_batch
+img_batch, label_batch = tf.train.shuffle_batch([img, label],
+                                                batch_size=4,
+                                                capacity=2000,
+                                                min_after_dequeue=1000,
+                                                num_threads=4)
+print("img_batch   : %s" % img_batch._shape)
+print("label_batch : %s" % label_batch._shape)
+init = tf.initialize_all_variables()
+with tf.Session() as sess:
+    sess.run(init)
+    threads = tf.train.start_queue_runners(sess=sess)
+    for i in range(30):  # number of mini-batch (step)
+        print("Step %d" % i)
+        val, l = sess.run([img_batch, label_batch])
+        print(val.shape, l)
+        tl.visualize.images2d(val, second=1, saveable=False, name='batch', dtype=None, fig_idx=2020121)
+    sess.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
