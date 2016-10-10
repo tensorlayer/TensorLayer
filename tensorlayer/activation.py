eb87783e024bc80f1e0105bc8a86bb616c3a9b3d
zsdonghao <dhsig552@163.com>
2016-10-10 21:12:33 +0100

    add leaky_relu

--- tensorlayer/activation.py
+++ tensorlayer/activation.py
@@ -65,12 +65,7 @@ def leaky_relu(x=None, alpha=0.1, name="LeakyReLU"):
 
     References
     ------------
-    - Rectifier Nonlinearities Improve Neural Network Acoustic Models,
-        Maas et al. (2013).
-
-    Links
-    --------
-    `Relu Hybrid ICML 2013 <http://web.stanford.edu/~awni/papers/relu_hybrid_icml2013_final.pdf>`_
+    `Rectifier Nonlinearities Improve Neural Network Acoustic Models, Maas et al. (2013) <http://web.stanford.edu/~awni/papers/relu_hybrid_icml2013_final.pdf>`_
 
     """
 
@@ -83,5 +78,49 @@ def leaky_relu(x=None, alpha=0.1, name="LeakyReLU"):
         m_x = tf.nn.relu(-x)
         x -= alpha * m_x
     # x.scope = scope
+
+    return x
+
+
+
+def prelu(x, channel_shared=False, W_init=tf.constant_initializer(value=0.0), W_init_args={}, restore=True, name="PReLU"):
+    """ PReLU.
+
+    Parametric Rectified Linear Unit.
+
+
+    Parameters
+    ----------
+    x : A `Tensor` with type `float`, `double`, `int32`, `int64`, `uint8`,
+        `int16`, or `int8`.
+    channel_shared : `bool`. Single weight is shared by all channels
+    W_init: weights initializer, default zero constant.
+        The initializer for initializing the alphas.
+    restore : `bool`. Restore or not alphas
+    name : A name for this activation op (optional).
+
+    Returns
+    -------
+    A `Tensor` with the same type as `x`.
+
+    References
+    -----------
+    Delving Deep into Rectifiers: Surpassing Human-Level Performance
+    on ImageNet Classification. He et al., 2014.
+
+    Links
+    -------
+    `Paper <http://arxiv.org/pdf/1502.01852v1.pdf>`_
+    """
+    print(' prelu: untested !!!')
+    if channel_shared:
+        w_shape = (1,)
+    else:
+        w_shape = int(x._shape[-1:])
+
+    with tf.name_scope(name) as scope:
+        W_init = initializations.get(weights_init)()
+        alphas = tf.get_variable(name='alphas', shape=w_shape, initializer=W_init, **W_init_args )
+        x = tf.nn.relu(x) + tf.mul(alphas, (x - tf.abs(x))) * 0.5
 
     return x
