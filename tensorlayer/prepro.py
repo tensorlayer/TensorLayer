c5ae8f18c372f1098bbd9048ef73e34228f311ca
zsdonghao <dhsig552@163.com>
2016-10-27 23:42:56 +0100

    [TYPO] -

--- tensorlayer/prepro.py
+++ tensorlayer/prepro.py
@@ -530,18 +530,20 @@ def brightness(x, gamma=1, gain=1, is_random=False):
         An image with dimension of [row, col, channel] (default).
     gamma : float, small than 1 means brighter.
         Non negative real number. Default value is 1.
-        If is_random is True, gamma in a range of (1-gamma, 1+gamma).
+
+        - If is_random is True, gamma in a range of (1-gamma, 1+gamma).
     gain : float
         The constant multiplier. Default value is 1.
     is_random : boolean, default False
-        If True, randomly change brightness.
+
+        - If True, randomly change brightness.
 
     References
     -----------
     - `skimage.exposure.adjust_gamma <http://scikit-image.org/docs/dev/api/skimage.exposure.html>`_
     - `chinese blog <http://www.cnblogs.com/denny402/p/5124402.html>`_
     """
-    if is_random:
+    andom:
         gamma = np.random.uniform(1-gamma, 1+gamma)
     x = exposure.adjust_gamma(x, gamma, gain)
     return x
