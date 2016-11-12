7b70db96e84a531099005e2ee762504d58e67dfa
zsdonghao <dhsig552@163.com>
2016-11-12 19:58:15 +0000

    release 1.2.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires = [
 
 setup(
     name = "tensorlayer",
-    version = "1.2.5",
+    version = "1.2.6",
     include_package_data=True,
     author='TensorLayer Contributors',
     author_email='hao.dong11@imperial.ac.uk',
