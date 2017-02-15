691884891b045471b9fecb9b72ffb8fd1b010f81
YinongLong <Yinong@longyinongdeMacBook-Pro.local>
2017-02-15 19:08:21 +0800

    Add a name
    There is an assertion about name, so I add this.

--- example/tutorial_mnist_simple.py
+++ example/tutorial_mnist_simple.py
@@ -33,7 +33,7 @@ network = tl.layers.DenseLayer(network, n_units=10,
 
 # define cost function and metric.
 y = network.outputs
-cost = tl.cost.cross_entropy(y, y_)
+cost = tl.cost.cross_entropy(y, y_, name='xentropy')
 correct_prediction = tf.equal(tf.argmax(y, 1), y_)
 acc = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 y_op = tf.argmax(tf.nn.softmax(y), 1)
