adf9432ba7c5c4e78cb8319b67e0f6df534858dd
Ross Marchant <ross.g.marchant@gmail.com>
2017-08-17 09:15:27 +0200

    Changed interpolation order from 0 to 3

--- tensorlayer/prepro.py
+++ tensorlayer/prepro.py
@@ -1140,7 +1140,7 @@ def apply_transform(x, transform_matrix, channel_index=2, fill_mode='nearest', c
     final_affine_matrix = transform_matrix[:2, :2]
     final_offset = transform_matrix[:2, 2]
     channel_images = [ndi.interpolation.affine_transform(x_channel, final_affine_matrix,
-                      final_offset, order=0, mode=fill_mode, cval=cval) for x_channel in x]
+                      final_offset, order=3, mode=fill_mode, cval=cval) for x_channel in x]
     x = np.stack(channel_images, axis=0)
     x = np.rollaxis(x, 0, channel_index+1)
     return x
