56f76d632443b7734c2b9f26cc10cb43487bf811
zsdonghao <dhsig552@163.com>
2017-08-31 18:08:40 +0100

    add ROI

--- tensorlayer/layers.py
+++ tensorlayer/layers.py
@@ -3625,6 +3625,48 @@ class PadLayer(Layer):
         self.all_drop = dict(layer.all_drop)
         self.all_layers.extend( [self.outputs] )
 
+## Object Detection
+class ROIPoolingLayer(Layer):
+    """
+    The :class:`ROIPoolingLayer` class is Region of interest pooling layer.
+
+    Parameters
+    -----------
+    layer : a :class:`Layer` instance
+        The `Layer` class feeding into this layer, the feature maps on which to perform the pooling operation
+    rois : list of regions of interest in the format (feature map index, upper left, bottom right)
+    pool_width : int, size of the pooling sections.
+    pool_width : int, size of the pooling sections.
+
+    References
+    -----------
+    - From `Deepsense-AI <https://github.com/deepsense-ai/roi-pooling>`_ .
+    """
+    def __init__(
+        self,
+        #inputs = None,
+        layer = None,
+        rois = None,
+        pool_height = 2,
+        pool_width = 2,
+        name = 'roipooling_layer',
+    ):
+        Layer.__init__(self, name=name)
+        self.inputs = layer.outputs
+        print ("  [TL] ROIPoolingLayer %s: (%d, %d)" % (self.name, pool_height, pool_width))
+        try:
+            from tensorlayer.third_party.roi_pooling.roi_pooling.roi_pooling_ops import roi_pooling
+        except Exception as e:
+            print(e)
+            print("\nHINT: \n1. https://github.com/deepsense-ai/roi-pooling  \n2. tensorlayer/third_party/roi_pooling\n")
+        self.outputs = roi_pooling(self.inputs, rois, pool_height, pool_width)
+
+        self.all_layers = list(layer.all_layers)
+        self.all_params = list(layer.all_params)
+        self.all_drop = dict(layer.all_drop)
+        self.all_layers.extend( [self.outputs] )
+
+
 ## TimeDistributedLayer
 class TimeDistributedLayer(Layer):
     """
