bba7ed8116d054987020fee92d5b8f4bee2a522a
Hao <dhsig552@163.com>
2017-09-29 11:54:00 +0100

    Update tutorial_mlp_dropout2.py

--- example/tutorial_mlp_dropout2.py
+++ example/tutorial_mlp_dropout2.py
@@ -21,18 +21,15 @@ def mlp(x, is_train=True, reuse=False):
         network = tl.layers.DropoutLayer(network, keep=0.8, is_fix=True,
                             is_train=is_train, name='drop1')
         network = tl.layers.DenseLayer(network, n_units=800,
-                                    act = tf.nn.relu, name='relu1')
-        # network = tl.layers.DropoutLayer(network, keep=0.5, name='drop2')
+                            act = tf.nn.relu, name='relu1')
         network = tl.layers.DropoutLayer(network, keep=0.5, is_fix=True,
                             is_train=is_train, name='drop2')
         network = tl.layers.DenseLayer(network, n_units=800,
-                                    act = tf.nn.relu, name='relu2')
-        # network = tl.layers.DropoutLayer(network, keep=0.5, name='drop3')
+                            act = tf.nn.relu, name='relu2')
         network = tl.layers.DropoutLayer(network, keep=0.5, is_fix=True,
                             is_train=is_train, name='drop3')
         network = tl.layers.DenseLayer(network, n_units=10,
-                                    act = tf.identity,
-                                    name='output')
+                            act = tf.identity, name='output')
     return network
 
 # define inferences
