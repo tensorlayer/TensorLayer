b715b7fc780e983273d0a0e0945d7f64392ec793
zsdonghao <dhsig552@163.com>
2017-11-04 01:33:20 +0000

    scope name - lrelu

--- tensorlayer/activation.py
+++ tensorlayer/activation.py
@@ -40,7 +40,7 @@ def ramp(x=None, v_min=0, v_max=1, name=None):
     """
     return tf.clip_by_value(x, clip_value_min=v_min, clip_value_max=v_max, name=name)
 
-def leaky_relu(x=None, alpha=0.1, name="LeakyReLU"):
+def leaky_relu(x=None, alpha=0.1, name="lrelu"):
     """The LeakyReLU, Shortcut is ``lrelu``.
 
     Modified version of ReLU, introducing a nonzero gradient for negative
@@ -63,11 +63,11 @@ def leaky_relu(x=None, alpha=0.1, name="LeakyReLU"):
     ------------
     - `Rectifier Nonlinearities Improve Neural Network Acoustic Models, Maas et al. (2013) <http://web.stanford.edu/~awni/papers/relu_hybrid_icml2013_final.pdf>`_
     """
-    with tf.name_scope(name) as scope:
+    # with tf.name_scope(name) as scope:
         # x = tf.nn.relu(x)
         # m_x = tf.nn.relu(-x)
         # x -= alpha * m_x
-        x = tf.maximum(x, alpha * x)
+    x = tf.maximum(x, alpha * x, name=name)
     return x
 
 #Shortcut
