fb1fea62e929d2ee7d60d9cbaf9783fadcfbd611
zsdonghao <dhsig552@163.com>
2017-11-13 15:46:42 +0000

    [release] tl.prepro.shear2

--- tensorlayer/prepro.py
+++ tensorlayer/prepro.py
@@ -443,6 +443,10 @@ def shear(x, intensity=0.1, is_random=False, row_index=0, col_index=1, channel_i
         The order of interpolation. The order has to be in the range 0-5. See ``apply_transform``.
 
         - `scipy ndimage affine_transform <https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.ndimage.interpolation.affine_transform.html>`_
+
+    References
+    -----------
+    - `Affine transformation <https://uk.mathworks.com/discovery/affine-transformation.html>`_
     """
     if is_random:
         shear = np.random.uniform(-intensity, intensity)
@@ -466,7 +470,7 @@ def shear_multi(x, intensity=0.1, is_random=False, row_index=0, col_index=1, cha
     -----------
     x : list of numpy array
         List of images with dimension of [n_images, row, col, channel] (default).
-    others : see ``shear``.
+    others : see ``tl.prepro.shear``.
     """
     if is_random:
         shear = np.random.uniform(-intensity, intensity)
@@ -474,6 +478,76 @@ def shear_multi(x, intensity=0.1, is_random=False, row_index=0, col_index=1, cha
         shear = intensity
     shear_matrix = np.array([[1, -np.sin(shear), 0],
                              [0, np.cos(shear), 0],
+                             [0, 0, 1]])
+
+    h, w = x[0].shape[row_index], x[0].shape[col_index]
+    transform_matrix = transform_matrix_offset_center(shear_matrix, h, w)
+    results = []
+    for data in x:
+        results.append( apply_transform(data, transform_matrix, channel_index, fill_mode, cval, order))
+    return np.asarray(results)
+
+def shear2(x, shear=(0.1, 0.1), is_random=False, row_index=0, col_index=1, channel_index=2,
+                 fill_mode='nearest', cval=0., order=1):
+    """Shear an image randomly or non-randomly.
+
+    Parameters
+    -----------
+    x : numpy array
+        An image with dimension of [row, col, channel] (default).
+    shear : tuple of two floats
+        Percentage of shear for height and width direction (0, 1).
+    is_random : boolean, default False
+        If True, randomly shear.
+    row_index, col_index, channel_index : int
+        Index of row, col and channel, default (0, 1, 2), for theano (1, 2, 0).
+    fill_mode : string
+        Method to fill missing pixel, default ‘nearest’, more options ‘constant’, ‘reflect’ or ‘wrap’.
+
+        - `scipy ndimage affine_transform <https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.ndimage.interpolation.affine_transform.html>`_
+    cval : scalar, optional
+        Value used for points outside the boundaries of the input if mode='constant'. Default is 0.0.
+    order : int, optional
+        The order of interpolation. The order has to be in the range 0-5. See ``apply_transform``.
+
+        - `scipy ndimage affine_transform <https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.ndimage.interpolation.affine_transform.html>`_
+
+    References
+    -----------
+    - `Affine transformation <https://uk.mathworks.com/discovery/affine-transformation.html>`_
+    """
+    assert len(shear) == 2, "shear should be tuple of 2 floats, or you want to use tl.prepro.shear rather than tl.prepro.shear2 ?"
+    if is_random:
+        shear[0] = np.random.uniform(-shear[0], -shear[0])
+        shear[1] = np.random.uniform(-shear[1], -shear[1])
+
+    shear_matrix = np.array([[1, shear[0], 0],
+                             [shear[1], 1, 0],
+                             [0, 0, 1]])
+
+    h, w = x.shape[row_index], x.shape[col_index]
+    transform_matrix = transform_matrix_offset_center(shear_matrix, h, w)
+    x = apply_transform(x, transform_matrix, channel_index, fill_mode, cval, order)
+    return x
+
+def shear_multi2(x, shear=(0.1, 0.1), is_random=False, row_index=0, col_index=1, channel_index=2,
+                 fill_mode='nearest', cval=0., order=1):
+    """Shear images with the same arguments, randomly or non-randomly.
+    Usually be used for image segmentation which x=[X, Y], X and Y should be matched.
+
+    Parameters
+    -----------
+    x : list of numpy array
+        List of images with dimension of [n_images, row, col, channel] (default).
+    others : see ``tl.prepro.shear2``.
+    """
+    assert len(shear) == 2, "shear should be tuple of 2 floats, or you want to use tl.prepro.shear_multi rather than tl.prepro.shear_multi2 ?"
+    if is_random:
+        shear[0] = np.random.uniform(-shear[0], -shear[0])
+        shear[1] = np.random.uniform(-shear[1], -shear[1])
+
+    shear_matrix = np.array([[1, shear[0], 0],
+                             [shear[1], 1, 0],
                              [0, 0, 1]])
 
     h, w = x[0].shape[row_index], x[0].shape[col_index]
