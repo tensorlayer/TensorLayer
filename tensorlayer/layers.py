ff6a16b5acc57971f832d2d513381f6df4885259
zsdonghao <dhsig552@163.com>
2017-11-25 00:09:33 +0000

    update docs

--- tensorlayer/layers.py
+++ tensorlayer/layers.py
@@ -4731,11 +4731,13 @@ class BasicConvLSTMCell(ConvRNNCell):
 
     @property
     def state_size(self):
+        """ State size of the LSTMStateTuple. """
         return (LSTMStateTuple(self._num_units, self._num_units)
                 if self._state_is_tuple else 2 * self._num_units)
 
     @property
     def output_size(self):
+        """ Number of units in outputs. """
         return self._num_units
 
     def __call__(self, inputs, state, scope=None):
