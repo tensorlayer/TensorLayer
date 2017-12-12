75ea2c586ae54279a463717414c2223a25470987
zsdonghao <dhsig552@163.com>
2017-12-12 15:53:48 +0000

    fix typo

--- tensorlayer/distributed.py
+++ tensorlayer/distributed.py
@@ -217,29 +217,29 @@ def DistributedSession(task_spec=None,
     Parameters
     ----------
     task_spec : TaskSpecDef. The task spec definition from TaskSpec()
-    checkpoint_dir: A string.  Optional path to a directory where to restore
+    checkpoint_dir : A string.  Optional path to a directory where to restore
       variables.
-    scaffold: A `Scaffold` used for gathering or building supportive ops. If
+    scaffold : A `Scaffold` used for gathering or building supportive ops. If
       not specified, a default one is created. It's used to finalize the graph.
-    hooks: Optional list of `SessionRunHook` objects.
-    chief_only_hooks: list of `SessionRunHook` objects. Activate these hooks if
+    hooks : Optional list of `SessionRunHook` objects.
+    chief_only_hooks : list of `SessionRunHook` objects. Activate these hooks if
       `is_chief==True`, ignore otherwise.
-    save_checkpoint_secs: The frequency, in seconds, that a checkpoint is saved
+    save_checkpoint_secs : The frequency, in seconds, that a checkpoint is saved
       using a default checkpoint saver. If `save_checkpoint_secs` is set to
       `None`, then the default checkpoint saver isn't used.
-    save_summaries_steps: The frequency, in number of global steps, that the
+    save_summaries_steps : The frequency, in number of global steps, that the
       summaries are written to disk using a default summary saver. If both
       `save_summaries_steps` and `save_summaries_secs` are set to `None`, then
       the default summary saver isn't used. Default 100.
-    save_summaries_secs: The frequency, in secs, that the summaries are written
+    save_summaries_secs : The frequency, in secs, that the summaries are written
       to disk using a default summary saver.  If both `save_summaries_steps` and
       `save_summaries_secs` are set to `None`, then the default summary saver
       isn't used. Default not enabled.
-    config: an instance of `tf.ConfigProto` proto used to configure the session.
+    config : an instance of `tf.ConfigProto` proto used to configure the session.
       It's the `config` argument of constructor of `tf.Session`.
-    stop_grace_period_secs: Number of seconds given to threads to stop after
+    stop_grace_period_secs : Number of seconds given to threads to stop after
       `close()` has been called.
-    log_step_count_steps: The frequency, in number of global steps, that the
+    log_step_count_steps : The frequency, in number of global steps, that the
       global step/sec is logged.
 
     References
