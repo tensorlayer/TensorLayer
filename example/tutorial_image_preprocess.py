bff06cc74e8adf6a884514092007bbd0d1283d24
zsdonghao <dhsig552@163.com>
2017-12-12 16:40:35 +0000

    udpate image prepro example

--- example/tutorial_image_preprocess.py
+++ example/tutorial_image_preprocess.py
@@ -15,12 +15,14 @@ X_train, y_train, X_test, y_test = tl.files.load_cifar10_dataset(
                                     shape=(-1, 32, 32, 3), plotable=False)
 
 def distort_img(x):
-    x = flip_axis(x, axis=1, is_random=True)
-    x = crop(x, wrg=28, hrg=28, is_random=True)
+    x = tl.prepro.flip_axis(x, axis=1, is_random=True)
+    x = tl.prepro.crop(x, wrg=28, hrg=28, is_random=True)
     return x
 
 s = time.time()
-results = threading_data(X_train[0:100], distort_img)
+results = tl.prepro.threading_data(X_train[0:100], distort_img)
 print("took %.3fs" % (time.time()-s))
 print(results.shape)
-tl.visualize.images2d(images=results[0:9], second=0.01, saveable=True, name='cifar10_distort', dtype=np.uint8)
+
+tl.vis.save_images(X_train[0:10], [1, 10], '_original.png')
+tl.vis.save_images(results[0:10], [1, 10], '_distorted.png')
