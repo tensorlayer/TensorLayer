27c431ab522b868af48e48c0e9f4080723457f2a
Jacky Wang <cnglen@gmail.com>
2017-12-17 23:03:52 +0800

    add pad_word if not in reverse_vocab in Vocabulary(); utf8 to utf-8; nlp.py style to pep8

--- tensorlayer/db.py
+++ tensorlayer/db.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 """
 Experimental Database Management System.
 
--- tensorlayer/layers.py
+++ tensorlayer/layers.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 
 import tensorflow as tf
 import time
