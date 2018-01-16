2381208caccce6afa6bbc6705cd04234377b0b64
Luo Mai <luo.mai.cs@gmail.com>
2018-01-16 21:53:44 +0800

    format visualize.py

--- tensorlayer/visualize.py
+++ tensorlayer/visualize.py
@@ -1,7 +1,6 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-
 import os
 
 import matplotlib
@@ -11,11 +10,9 @@ import scipy.misc
 
 from . import prepro
 
-
 ## use this, if you got the following error:
 #  _tkinter.TclError: no display name and no $DISPLAY environment variable
 # matplotlib.use('Agg')
-
 
 
 def read_image(image, path=''):
@@ -27,6 +24,7 @@ def read_image(image, path=''):
     path : string, path.
     """
     return scipy.misc.imread(os.path.join(path, image))
+
 
 def read_images(img_list, path='', n_threads=10, printable=True):
     """ Returns all images in list by given path and name of each image file.
@@ -40,13 +38,14 @@ def read_images(img_list, path='', n_threads=10, printable=True):
     """
     imgs = []
     for idx in range(0, len(img_list), n_threads):
-        b_imgs_list = img_list[idx : idx + n_threads]
+        b_imgs_list = img_list[idx:idx + n_threads]
         b_imgs = prepro.threading_data(b_imgs_list, fn=read_image, path=path)
         # print(b_imgs.shape)
         imgs.extend(b_imgs)
         if printable:
             print('read %d from %s' % (len(imgs), path))
     return imgs
+
 
 def save_image(image, image_path=''):
     """Save one image.
@@ -56,10 +55,11 @@ def save_image(image, image_path=''):
     images : numpy array [w, h, c]
     image_path : string.
     """
-    try: # RGB
+    try:  # RGB
         scipy.misc.imsave(image_path, image)
-    except: # Greyscale
-        scipy.misc.imsave(image_path, image[:,:,0])
+    except:  # Greyscale
+        scipy.misc.imsave(image_path, image[:, :, 0])
+
 
 def save_images(images, size, image_path=''):
     """Save mutiple images into one single image.
@@ -76,13 +76,14 @@ def save_images(images, size, image_path=''):
     >>> images = np.random.rand(64, 100, 100, 3)
     >>> tl.visualize.save_images(images, [8, 8], 'temp.png')
     """
+
     def merge(images, size):
         h, w = images.shape[1], images.shape[2]
         img = np.zeros((h * size[0], w * size[1], 3))
         for idx, image in enumerate(images):
             i = idx % size[1]
             j = idx // size[1]
-            img[j*h:j*h+h, i*w:i*w+w, :] = image
+            img[j * h:j * h + h, i * w:i * w + w, :] = image
         return img
 
     def imsave(images, size, path):
@@ -91,10 +92,9 @@ def save_images(images, size, image_path=''):
     assert len(images) <= size[0] * size[1], "number of images should be equal or less than size[0] * size[1] {}".format(len(images))
     return imsave(images, size, image_path)
 
+
 # for object detection
-def draw_boxes_and_labels_to_image(image, classes=[], coords=[],
-                scores=[], classes_list=[],
-                is_center=True, is_rescale=True, save_name=None):
+def draw_boxes_and_labels_to_image(image, classes=[], coords=[], scores=[], classes_list=[], is_center=True, is_rescale=True, save_name=None):
     """ Draw bboxes and class labels on image. Return or save the image with bboxes, example in the docs of ``tl.prepro``.
 
     Parameters
@@ -126,8 +126,8 @@ def draw_boxes_and_labels_to_image(image, classes=[], coords=[],
 
     import cv2
 
-        # image = copy.copy(image)    # don't change the original image
-    image = image.copy()    # don't change the original image, and avoid error https://stackoverflow.com/questions/30249053/python-opencv-drawing-errors-after-manipulating-array-with-numpy
+    # don't change the original image, and avoid error https://stackoverflow.com/questions/30249053/python-opencv-drawing-errors-after-manipulating-array-with-numpy
+    image = image.copy()
 
     imh, imw = image.shape[0:2]
     thick = int((imh + imw) // 430)
@@ -138,22 +138,24 @@ def draw_boxes_and_labels_to_image(image, classes=[], coords=[],
         else:
             x, y, x2, y2 = coords[i]
 
-        if is_rescale: # scale back to pixel unit if the coords are the portion of width and high
+        if is_rescale:  # scale back to pixel unit if the coords are the portion of width and high
             x, y, x2, y2 = prepro.obj_box_coord_scale_to_pixelunit([x, y, x2, y2], (imh, imw))
 
-        cv2.rectangle(image,
-            (int(x), int(y)), (int(x2), int(y2)),   # up-left and botton-right
-            [0,255,0],
+        cv2.rectangle(
+            image,
+            (int(x), int(y)),
+            (int(x2), int(y2)),  # up-left and botton-right
+            [0, 255, 0],
             thick)
 
         cv2.putText(
             image,
             classes_list[classes[i]] + ((" %.2f" % (scores[i])) if (len(scores) != 0) else " "),
-            (int(x), int(y)),   # button left
+            (int(x), int(y)),  # button left
             0,
-            1.5e-3 * imh,       # bigger = larger font
-            [0,0,256],          # self.meta['colors'][max_indx],
-            int(thick/2)+1)     # bold
+            1.5e-3 * imh,  # bigger = larger font
+            [0, 0, 256],  # self.meta['colors'][max_indx],
+            int(thick / 2) + 1)  # bold
 
     if save_name is not None:
         # cv2.imwrite('_my.png', image)
@@ -162,8 +164,9 @@ def draw_boxes_and_labels_to_image(image, classes=[], coords=[],
     #     print("draw_boxes_and_labels_to_image: no bboxes exist, cannot draw !")
     return image
 
+
 # old APIs
-def W(W=None, second=10, saveable=True, shape=[28,28], name='mnist', fig_idx=2396512):
+def W(W=None, second=10, saveable=True, shape=[28, 28], name='mnist', fig_idx=2396512):
     """Visualize every columns of the weight matrix to a group of Greyscale img.
 
     Parameters
@@ -188,22 +191,22 @@ def W(W=None, second=10, saveable=True, shape=[28,28], name='mnist', fig_idx=239
     import matplotlib.pyplot as plt
     if saveable is False:
         plt.ion()
-    fig = plt.figure(fig_idx)      # show all feature images
+    fig = plt.figure(fig_idx)  # show all feature images
     size = W.shape[0]
     n_units = W.shape[1]
 
     num_r = int(np.sqrt(n_units))  # 每行显示的个数   若25个hidden unit -> 每行显示5个
-    num_c = int(np.ceil(n_units/num_r))
+    num_c = int(np.ceil(n_units / num_r))
     count = int(1)
-    for row in range(1, num_r+1):
-        for col in range(1, num_c+1):
+    for row in range(1, num_r + 1):
+        for col in range(1, num_c + 1):
             if count > n_units:
                 break
             a = fig.add_subplot(num_r, num_c, count)
             # ------------------------------------------------------------
             # plt.imshow(np.reshape(W[:,count-1],(28,28)), cmap='gray')
             # ------------------------------------------------------------
-            feature = W[:,count-1] / np.sqrt( (W[:,count-1]**2).sum())
+            feature = W[:, count - 1] / np.sqrt((W[:, count - 1]**2).sum())
             # feature[feature<0.0001] = 0   # value threshold
             # if count == 1 or count == 2:
             #     print(np.mean(feature))
@@ -211,19 +214,19 @@ def W(W=None, second=10, saveable=True, shape=[28,28], name='mnist', fig_idx=239
             #     feature = np.zeros_like(feature)
             # if np.mean(feature) < -0.015:      # condition threshold
             #     feature = np.zeros_like(feature)
-            plt.imshow(np.reshape(feature ,(shape[0],shape[1])),
-                    cmap='gray', interpolation="nearest")#, vmin=np.min(feature), vmax=np.max(feature))
+            plt.imshow(np.reshape(feature, (shape[0], shape[1])), cmap='gray', interpolation="nearest")  #, vmin=np.min(feature), vmax=np.max(feature))
             # plt.title(name)
             # ------------------------------------------------------------
             # plt.imshow(np.reshape(W[:,count-1] ,(np.sqrt(size),np.sqrt(size))), cmap='gray', interpolation="nearest")
-            plt.gca().xaxis.set_major_locator(plt.NullLocator())    # distable tick
+            plt.gca().xaxis.set_major_locator(plt.NullLocator())  # distable tick
             plt.gca().yaxis.set_major_locator(plt.NullLocator())
             count = count + 1
     if saveable:
-        plt.savefig(name+'.pdf',format='pdf')
+        plt.savefig(name + '.pdf', format='pdf')
     else:
         plt.draw()
         plt.pause(second)
+
 
 def frame(I=None, second=5, saveable=True, name='frame', cmap=None, fig_idx=12836):
     """Display a frame(image). Make sure OpenAI Gym render() is disable before using it.
@@ -252,10 +255,10 @@ def frame(I=None, second=5, saveable=True, name='frame', cmap=None, fig_idx=1283
     import matplotlib.pyplot as plt
     if saveable is False:
         plt.ion()
-    fig = plt.figure(fig_idx)      # show all feature images
+    fig = plt.figure(fig_idx)  # show all feature images
 
-    if len(I.shape) and I.shape[-1]==1:     # (10,10,1) --> (10,10)
-        I = I[:,:,0]
+    if len(I.shape) and I.shape[-1] == 1:  # (10,10,1) --> (10,10)
+        I = I[:, :, 0]
 
     plt.imshow(I, cmap)
     plt.title(name)
@@ -263,10 +266,11 @@ def frame(I=None, second=5, saveable=True, name='frame', cmap=None, fig_idx=1283
     # plt.gca().yaxis.set_major_locator(plt.NullLocator())
 
     if saveable:
-        plt.savefig(name+'.pdf',format='pdf')
+        plt.savefig(name + '.pdf', format='pdf')
     else:
         plt.draw()
         plt.pause(second)
+
 
 def CNN2d(CNN=None, second=10, saveable=True, name='cnn', fig_idx=3119362):
     """Display a group of RGB or Greyscale CNN masks.
@@ -296,12 +300,12 @@ def CNN2d(CNN=None, second=10, saveable=True, name='cnn', fig_idx=3119362):
     n_col = CNN.shape[1]
     n_color = CNN.shape[2]
     row = int(np.sqrt(n_mask))
-    col = int(np.ceil(n_mask/row))
-    plt.ion()   # active mode
+    col = int(np.ceil(n_mask / row))
+    plt.ion()  # active mode
     fig = plt.figure(fig_idx)
     count = 1
-    for ir in range(1, row+1):
-        for ic in range(1, col+1):
+    for ir in range(1, row + 1):
+        for ic in range(1, col + 1):
             if count > n_mask:
                 break
             a = fig.add_subplot(col, row, count)
@@ -311,26 +315,22 @@ def CNN2d(CNN=None, second=10, saveable=True, name='cnn', fig_idx=3119362):
             #         np.reshape(CNN[count-1,:,:,:], (n_row, n_col)),
             #         cmap='gray', interpolation="nearest")     # theano
             if n_color == 1:
-                plt.imshow(
-                        np.reshape(CNN[:,:,:,count-1], (n_row, n_col)),
-                        cmap='gray', interpolation="nearest")
+                plt.imshow(np.reshape(CNN[:, :, :, count - 1], (n_row, n_col)), cmap='gray', interpolation="nearest")
             elif n_color == 3:
-                plt.imshow(
-                        np.reshape(CNN[:,:,:,count-1], (n_row, n_col, n_color)),
-                        cmap='gray', interpolation="nearest")
+                plt.imshow(np.reshape(CNN[:, :, :, count - 1], (n_row, n_col, n_color)), cmap='gray', interpolation="nearest")
             else:
                 raise Exception("Unknown n_color")
-            plt.gca().xaxis.set_major_locator(plt.NullLocator())    # distable tick
+            plt.gca().xaxis.set_major_locator(plt.NullLocator())  # distable tick
             plt.gca().yaxis.set_major_locator(plt.NullLocator())
             count = count + 1
     if saveable:
-        plt.savefig(name+'.pdf',format='pdf')
+        plt.savefig(name + '.pdf', format='pdf')
     else:
         plt.draw()
         plt.pause(second)
 
-def images2d(images=None, second=10, saveable=True, name='images', dtype=None,
-                                                            fig_idx=3119362):
+
+def images2d(images=None, second=10, saveable=True, name='images', dtype=None, fig_idx=3119362):
     """Display a group of RGB or Greyscale images.
 
     Parameters
@@ -363,12 +363,12 @@ def images2d(images=None, second=10, saveable=True, name='images', dtype=None,
     n_col = images.shape[2]
     n_color = images.shape[3]
     row = int(np.sqrt(n_mask))
-    col = int(np.ceil(n_mask/row))
-    plt.ion()   # active mode
+    col = int(np.ceil(n_mask / row))
+    plt.ion()  # active mode
     fig = plt.figure(fig_idx)
     count = 1
-    for ir in range(1, row+1):
-        for ic in range(1, col+1):
+    for ir in range(1, row + 1):
+        for ic in range(1, col + 1):
             if count > n_mask:
                 break
             a = fig.add_subplot(col, row, count)
@@ -377,27 +377,24 @@ def images2d(images=None, second=10, saveable=True, name='images', dtype=None,
             #         np.reshape(images[count-1,:,:,:], (n_row, n_col)),
             #         cmap='gray', interpolation="nearest")     # theano
             if n_color == 1:
-                plt.imshow(
-                        np.reshape(images[count-1,:,:], (n_row, n_col)),
-                        cmap='gray', interpolation="nearest")
+                plt.imshow(np.reshape(images[count - 1, :, :], (n_row, n_col)), cmap='gray', interpolation="nearest")
                 # plt.title(name)
             elif n_color == 3:
-                plt.imshow(images[count-1,:,:],
-                        cmap='gray', interpolation="nearest")
+                plt.imshow(images[count - 1, :, :], cmap='gray', interpolation="nearest")
                 # plt.title(name)
             else:
                 raise Exception("Unknown n_color")
-            plt.gca().xaxis.set_major_locator(plt.NullLocator())    # distable tick
+            plt.gca().xaxis.set_major_locator(plt.NullLocator())  # distable tick
             plt.gca().yaxis.set_major_locator(plt.NullLocator())
             count = count + 1
     if saveable:
-        plt.savefig(name+'.pdf',format='pdf')
+        plt.savefig(name + '.pdf', format='pdf')
     else:
         plt.draw()
         plt.pause(second)
 
-def tsne_embedding(embeddings, reverse_dictionary, plot_only=500,
-                        second=5, saveable=False, name='tsne', fig_idx=9862):
+
+def tsne_embedding(embeddings, reverse_dictionary, plot_only=500, second=5, saveable=False, name='tsne', fig_idx=9862):
     """Visualize the embeddings by using t-SNE.
 
     Parameters
@@ -425,24 +422,19 @@ def tsne_embedding(embeddings, reverse_dictionary, plot_only=500,
     ...                   plot_only=500, second=5, saveable=False, name='tsne')
     """
     import matplotlib.pyplot as plt
-    def plot_with_labels(low_dim_embs, labels, figsize=(18, 18), second=5,
-                                    saveable=True, name='tsne', fig_idx=9862):
+
+    def plot_with_labels(low_dim_embs, labels, figsize=(18, 18), second=5, saveable=True, name='tsne', fig_idx=9862):
         assert low_dim_embs.shape[0] >= len(labels), "More labels than embeddings"
         if saveable is False:
             plt.ion()
             plt.figure(fig_idx)
         plt.figure(figsize=figsize)  #in inches
         for i, label in enumerate(labels):
-            x, y = low_dim_embs[i,:]
+            x, y = low_dim_embs[i, :]
             plt.scatter(x, y)
-            plt.annotate(label,
-                     xy=(x, y),
-                     xytext=(5, 2),
-                     textcoords='offset points',
-                     ha='right',
-                     va='bottom')
+            plt.annotate(label, xy=(x, y), xytext=(5, 2), textcoords='offset points', ha='right', va='bottom')
         if saveable:
-            plt.savefig(name+'.pdf',format='pdf')
+            plt.savefig(name + '.pdf', format='pdf')
         else:
             plt.draw()
             plt.pause(second)
@@ -454,12 +446,9 @@ def tsne_embedding(embeddings, reverse_dictionary, plot_only=500,
 
         tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
         # plot_only = 500
-        low_dim_embs = tsne.fit_transform(embeddings[:plot_only,:])
+        low_dim_embs = tsne.fit_transform(embeddings[:plot_only, :])
         labels = [reverse_dictionary[i] for i in xrange(plot_only)]
         plot_with_labels(low_dim_embs, labels, second=second, saveable=saveable, \
                                                     name=name, fig_idx=fig_idx)
     except ImportError:
         print("Please install sklearn and matplotlib to visualize embeddings.")
-
-
-#
