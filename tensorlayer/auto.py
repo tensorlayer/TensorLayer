f04ddfca01b3528200217888968cf095439fab7e
lg <lg4869@outlook.com>
2018-01-21 21:19:40 +0800

    use tl.distributed

--- tensorlayer/auto.py
+++ tensorlayer/auto.py
@@ -2,17 +2,12 @@ import json
 import os
 
 import tensorflow as tf
+from . import distributed
+ 
 
-
-def create_session():
-    tf_config = os.environ.get('TF_CONFIG')
-    if not tf_config:
-        return tf.InteractiveSession()
-    # TODO: use tl.distributed
-    tf_config = json.loads(tf_config)
-    cluster = tf.train.ClusterSpec(tf_config['cluster'])
-    task = tf_config['task']
-    server = tf.train.Server(
-        cluster, job_name=task['type'], task_index=task['index'])
-    sess = tf.Session(server.target)
-    return sess
+def Session():
+    task_spec = distributed.TaskSpec()
+    if not task_spec:
+        return tf.InteractiveSession().as_default()
+    task_spec.create_server()
+    return distributed.DistributedSession(task_spec=task_spec)
