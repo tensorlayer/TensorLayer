2437a8ea737f173af9e3c55ab2d1cefd7a068573
Luo Mai <luo.mai.cs@gmail.com>
2018-01-24 01:03:05 +0800

    validate input parameters and handle exit signals for #286

--- experimental/local_trainer.py
+++ experimental/local_trainer.py
@@ -5,9 +5,13 @@ import argparse
 import json
 import os
 import subprocess
+import multiprocessing
+import signal
+import sys
+from tensorflow.python.client import device_lib
+
 
 PORT_BASE = 10000
-ENABLE_GPU = False  # Set to False only for local testing
 
 
 def create_tf_config_str(cluster_spec, task_type, task_index):
@@ -19,15 +23,15 @@ def create_tf_config_str(cluster_spec, task_type, task_index):
     return json.dumps(full_spec)
 
 
-def run_workers(cluster_spec, file):
+def run_workers(cluster_spec, enable_gpu, file):
     processes = []
     gpu_id = 0  # Assume GPU device id starts from 0
     for task_index in range(0, len(cluster_spec['cluster']['worker'])):
         add_env = dict()
 
-        if ENABLE_GPU:
+        if enable_gpu == 1:
             add_env['CUDA_VISIBLE_DEVICES'] = str(gpu_id)
-            gpu_id = gpu_id + 1
+            gpu_id += 1
         else:
             add_env['CUDA_VISIBLE_DEVICES'] = ''  # Worker uses CPU if GPU is not available
 
@@ -58,29 +62,63 @@ def run_parameter_servers(cluster_spec, file):
     return processes
 
 
+def get_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos if x.device_type == 'GPU']
+
+
+def validate_arguments(args):
+    if args.num_pss < 1:
+        print('Value error: must have ore than one parameter servers.')
+        exit(1)
+        
+    if args.enable_gpu == 1:
+        num_gpus = len(get_available_gpus())
+        if args.num_workers > num_gpus:
+            print('Value error: there are %s available GPUs but you are requiring %s.' % (num_gpus, args.num_workers))
+            exit(1)
+    else:
+        num_cpus = multiprocessing.cpu_count()
+        if args.num_workers > num_cpus:
+            print('Value error: there are %s available CPUs but you are requiring %s.' % (num_cpus, args.num_workers))
+            exit(1)
+
+    if os.path.isfile(args.file) == False:
+        print('Value error: model trainning file does not exist')
+        exit(1)
+
+
+def signal_handler(signal, frame):
+    print('You pressed Ctrl+C!')
+    for p in child_process:
+        p.kill()
+    sys.exit(0)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--pss", help="number of parameter servers")
-    parser.add_argument("-w", "--workers", help="number of workers")
-    parser.add_argument("-f", "--file", help="file path")
+    parser.add_argument('-p', '--pss', dest='num_pss', type=int, default=1, help='number of parameter servers')
+    parser.add_argument('-w', '--workers', dest='num_workers', type=int, help='number of workers')
+    parser.add_argument('-g', '--enable_gpu', dest='enable_gpu', type=int, default=0, help='set to 1 to enable GPU (GPU and CPU cannot be enabled together to avoid stragglers)')
+    parser.add_argument('-f', '--file', dest='file', help='model trainning file path')
     args = parser.parse_args()
+
+    validate_arguments(args)
 
     cluster_spec = {
         'cluster': {
-            'ps': ['localhost:' + str(PORT_BASE + i) for i in range(0, int(args.pss))],
-            'worker': ['localhost:' + str(PORT_BASE + int(args.pss) + i) for i in range(0, int(args.workers))]
+            'ps': ['localhost:' + str(PORT_BASE + i) for i in range(0, args.num_pss)],
+            'worker': ['localhost:' + str(PORT_BASE + args.num_pss + i) for i in range(0, args.num_workers)]
         }
     }
 
-    pss = run_parameter_servers(cluster_spec, args.file)
-    workers = run_workers(cluster_spec, args.file)
-
-    input("Press Enter to exit...\n")
-
-    for p in pss:
-        p.kill()
-
-    for p in workers:
-        p.kill()
-
-    print("END")
+    processes = []
+    try:
+        processes.extend(run_parameter_servers(cluster_spec, args.file))
+        processes.extend(run_workers(cluster_spec, args.enable_gpu, args.file))
+        input('Press ENTER to exit the training ...')
+    except KeyboardInterrupt:
+        print('Keyboard interrupt received, stopping…')
+    finally:
+        # clean up
+        for p in processes:
+            p.kill()
