cce622358d762b4a2b45211613c52c1946b8b383
lg <lg4869@outlook.com>
2018-01-29 21:01:25 +0800

    make local_trainer aware of CUDA_VISIBLE_DEVICES and derive GPU ids from it (#286)

--- experimental/local_trainer.py
+++ experimental/local_trainer.py
@@ -15,8 +15,15 @@ import sys
 PORT_BASE = 10000
 
 
-def get_gpus():
-    return [d for d in os.listdir('/dev') if re.match('^nvidia\d+$', d)]
+def _get_gpu_ids():
+    available_gpu_ids = [int(d.replace('nvidia', '')) for d in os.listdir('/dev') if re.match('^nvidia\d+$', d)]
+    if 'CUDA_VISIBLE_DEVICES' in os.environ:
+        visiable_gpu_ids = [int(x) for x in os.environ.get('CUDA_VISIBLE_DEVICES', '').split(',')]
+        available_gpu_ids = list(set(available_gpu_ids) & set(visiable_gpu_ids))
+    return available_gpu_ids
+
+
+GPU_IDS = _get_gpu_ids()
 
 
 def create_tf_config(cluster_spec, task_type, task_index):
@@ -29,12 +36,13 @@ def create_tf_config(cluster_spec, task_type, task_index):
     }
 
 
-def create_tf_jobs(prog, cluster_spec, enable_gpu=False):
+def create_tf_jobs(prog, cluster_spec):
+    gpu_assignment = dict((('worker', idx), gpu_idx) for (idx, gpu_idx) in enumerate(GPU_IDS))
     for job_type in cluster_spec:
         for task_index in range(len(cluster_spec[job_type])):
             new_env = os.environ.copy()
             new_env.update({
-                'CUDA_VISIBLE_DEVICES': str(task_index) if job_type == 'worker' and enable_gpu else '',
+                'CUDA_VISIBLE_DEVICES': str(gpu_assignment.get((job_type, task_index), '')),
                 'TF_CONFIG': json.dumps(create_tf_config(cluster_spec, job_type, task_index)),
             })
             yield subprocess.Popen(['python3', prog], env=new_env)
@@ -45,8 +53,8 @@ def validate_arguments(args):
         print('Value error: must have ore than one parameter servers.')
         exit(1)
 
-    if args.enable_gpu:
-        num_gpus = len(get_gpus())
+    if GPU_IDS:
+        num_gpus = len(GPU_IDS)
         if args.num_workers > num_gpus:
             print('Value error: there are %s available GPUs but you are requiring %s.' % (num_gpus, args.num_workers))
             exit(1)
@@ -64,20 +72,18 @@ def validate_arguments(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--pss', dest='num_pss', type=int, default=1, help='number of parameter servers')
-    parser.add_argument('-w', '--workers', dest='num_workers', type=int, required=True, help='number of workers')
-    parser.add_argument(
-        '-g', '--enable_gpu', dest='enable_gpu', action='store_true', help='enable GPU (GPU and CPU are NOT enabled together to avoid stragglers)')
+    parser.add_argument('-w', '--workers', dest='num_workers', type=int, default=len(GPU_IDS) if GPU_IDS else 1, help='number of workers')
     parser.add_argument('-f', '--file', dest='file', help='model trainning file path')
     args = parser.parse_args()
 
     validate_arguments(args)
-
+    print('Using %d workers, %d parameter servers, %d GPUs.' % (args.num_workers, args.num_pss, len(GPU_IDS)))
     cluster_spec = {
         'ps': ['localhost:%d' % (PORT_BASE + i) for i in range(args.num_pss)],
         'worker': ['localhost:%d' % (PORT_BASE + args.num_pss + i) for i in range(args.num_workers)]
     }
 
-    processes = list(create_tf_jobs(args.file, cluster_spec, args.enable_gpu))
+    processes = list(create_tf_jobs(args.file, cluster_spec))
     try:
         print('Press ENTER to exit the training ...')
         sys.stdin.readline()
@@ -87,4 +93,6 @@ if __name__ == "__main__":
         print('stopping all subprocesses ...')
         for p in processes:
             p.kill()
+        for p in processes:
+            p.wait()
         print('END')
