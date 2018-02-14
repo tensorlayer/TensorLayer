0fa17af607e026417895a072982e5c1b6c6bf649
zsdonghao <dhsig552@163.com>
2018-02-14 14:20:32 +0000

    rename files

--- tensorlayer/layers/__init__.py
+++ tensorlayer/layers/__init__.py
@@ -1,14 +1,14 @@
 
 # from . import core
 from .core import *
-from .conv_pro import *
-from .conv_simple import *
+from .convolution_pro import *
+from .convolution_simple import *
 from .super_resolution import *
-from .normalize import *
+from .normalization import *
 from .spatial_transformer import *
 from .object_detection import *
-from .time_distributed import *
-from .poolpad import *
+from .time_distribution import *
+from .pooling_padding import *
 from .recurrent import *
 from .shape import *
 from .lambda_merge_extend_stack import *
