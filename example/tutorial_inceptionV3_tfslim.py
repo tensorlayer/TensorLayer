365f0222f74c18f128608239b0b1c392c7705328
lg <lg4869@outlook.com>
2018-02-24 00:04:31 +0800

    run yapf after isort

--- example/tutorial_inceptionV3_tfslim.py
+++ example/tutorial_inceptionV3_tfslim.py
@@ -34,8 +34,7 @@ import tensorflow as tf
 import tensorlayer as tl
 # from scipy.misc import imread, imresize
 # from tensorflow.contrib.slim.python.slim.nets.alexnet import alexnet_v2
-from tensorflow.contrib.slim.python.slim.nets.inception_v3 import (inception_v3,  # , inception_v3_base)
-                                                                   inception_v3_arg_scope)
+from tensorflow.contrib.slim.python.slim.nets.inception_v3 import (inception_v3, inception_v3_arg_scope)
 
 slim = tf.contrib.slim
 try:
