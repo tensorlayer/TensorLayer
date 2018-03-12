ddff635d9eadd28d509f0d4fcd8b3f18a4c0f945
AutuanLiu <autuanliu@163.com>
2018-03-12 11:39:23 +0800

    fashion-MNIST test

--- tests/test_mnist_simple.py
+++ tests/test_mnist_simple.py
@@ -8,6 +8,8 @@ sess = tf.InteractiveSession()
 
 # prepare data
 X_train, y_train, X_val, y_val, X_test, y_test = tl.files.load_mnist_dataset(shape=(-1, 784))
+# for fashion_MNIST dataset test
+# X_train, y_train, X_val, y_val, X_test, y_test = tl.files.load_fashion_mnist_dataset(shape=(-1, 784))
 # define placeholder
 x = tf.placeholder(tf.float32, shape=[None, 784], name='x')
 y_ = tf.placeholder(tf.int64, shape=[None], name='y_')
