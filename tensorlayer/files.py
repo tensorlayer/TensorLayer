f24d87edbc138b82183c5407793bc274ebfeff01
AutuanLiu <autuanliu@163.com>
2018-03-12 11:28:34 +0800

    add support for fashion_MNIST dataset

--- tensorlayer/files.py
+++ tensorlayer/files.py
@@ -117,6 +117,73 @@ def load_mnist_dataset(shape=(-1, 784), path="data"):
     return X_train, y_train, X_val, y_val, X_test, y_test
 
 
+def load_fashion_mnist_dataset(shape=(-1, 784), path="data"):
+    """Load fashion-MNIST dataset.(https://github.com/zalandoresearch/fashion-mnist)
+
+    Automatically download fashion-MNIST dataset and return the training, validation and test set with 50000, 10000 and 10000 digit images respectively.
+
+    Parameters
+    ----------
+    shape : tuple
+        The shape of digit images e.g. (-1,784) or (-1, 28, 28, 1).
+    path : str
+        The path that the data is downloaded to, defaults is ``data/fashionMnist/``.
+
+    Examples
+    --------
+    >>> X_train, y_train, X_val, y_val, X_test, y_test = tl.files.load_fashion_mnist_dataset(shape=(-1,784))
+    >>> X_train, y_train, X_val, y_val, X_test, y_test = tl.files.load_fashion_mnist_dataset(shape=(-1, 28, 28, 1))
+
+    """
+    path = os.path.join(path, 'fashionMnist')
+
+    # We first define functions for loading fashion-MNIST images and labels.
+    # For convenience, they also download the requested files if needed.
+    def load_mnist_images(path, filename):
+        filepath = maybe_download_and_extract(filename, path, 'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/')
+
+        logging.info(filepath)
+        # Read the inputs in Yann LeCun's binary format.
+        with gzip.open(filepath, 'rb') as f:
+            data = np.frombuffer(f.read(), np.uint8, offset=16)
+        # The inputs are vectors now, we reshape them to monochrome 2D images,
+        # following the shape convention: (examples, channels, rows, columns)
+        data = data.reshape(shape)
+        # The inputs come as bytes, we convert them to float32 in range [0,1].
+        # (Actually to range [0, 255/256], for compatibility to the version
+        # provided at http://deeplearning.net/data/mnist/mnist.pkl.gz.)
+        return data / np.float32(256)
+
+    def load_mnist_labels(path, filename):
+        filepath = maybe_download_and_extract(filename, path, 'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/')
+        # Read the labels in Yann LeCun's binary format.
+        with gzip.open(filepath, 'rb') as f:
+            data = np.frombuffer(f.read(), np.uint8, offset=8)
+        # The labels are vectors of integers now, that's exactly what we want.
+        return data
+
+    # Download and read the training and test set images and labels.
+    logging.info("Load or Download fashion-MNIST > {}".format(path))
+    X_train = load_mnist_images(path, 'train-images-idx3-ubyte.gz')
+    y_train = load_mnist_labels(path, 'train-labels-idx1-ubyte.gz')
+    X_test = load_mnist_images(path, 't10k-images-idx3-ubyte.gz')
+    y_test = load_mnist_labels(path, 't10k-labels-idx1-ubyte.gz')
+
+    # We reserve the last 10000 training examples for validation.
+    X_train, X_val = X_train[:-10000], X_train[-10000:]
+    y_train, y_val = y_train[:-10000], y_train[-10000:]
+
+    # We just return all the arrays in order, as expected in main().
+    # (It doesn't matter how we do this as long as we can read them again.)
+    X_train = np.asarray(X_train, dtype=np.float32)
+    y_train = np.asarray(y_train, dtype=np.int32)
+    X_val = np.asarray(X_val, dtype=np.float32)
+    y_val = np.asarray(y_val, dtype=np.int32)
+    X_test = np.asarray(X_test, dtype=np.float32)
+    y_test = np.asarray(y_test, dtype=np.int32)
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+
 def load_cifar10_dataset(shape=(-1, 32, 32, 3), path='data', plotable=False):
     """Load CIFAR-10 dataset.
 
