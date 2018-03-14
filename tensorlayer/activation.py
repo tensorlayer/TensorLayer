0ab26b78d97b3922fd95c3857d2b19d814d1249b
zsdonghao <dhsig552@163.com>
2018-03-14 18:31:56 +0000

    mark deprecated activation API

--- tensorlayer/activation.py
+++ tensorlayer/activation.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import tensorflow as tf
+from tensorflow.python.util.deprecation import deprecated
 
 __all__ = [
     'identity',
@@ -14,6 +15,7 @@ __all__ = [
 ]
 
 
+@deprecated("2018-06-30", "This API will be deprecated soon as tf.identity can do the same thing.")
 def identity(x):
     """The identity activation function.
     Shortcut is ``linear``.
@@ -114,6 +116,7 @@ def swish(x, name='swish'):
     return x
 
 
+@deprecated("2018-06-30", "This API will be deprecated soon as tf.nn.softmax can do the same thing.")
 def pixel_wise_softmax(x, name='pixel_wise_softmax'):
     """Return the softmax outputs of images, every pixels have multiple label, the sum of a pixel is 1.
     Usually be used for image segmentation.
