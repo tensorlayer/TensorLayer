5f20b6bd2812576c7bf8f3e1b66682b7d195fae0
zsdonghao <dhsig552@163.com>
2018-03-19 18:30:37 +0000

    release zeropad 1d 2d 3d

---
+++ tests/test_layers_padding.py
@@ -0,0 +1,59 @@
+import tensorflow as tf
+from tensorlayer.layers import ZeroPad1d, ZeroPad2d, ZeroPad3d
+
+## 1D
+x = tf.placeholder(tf.float32, (None, 100, 1))
+n = InputLayer(x)
+n1 = ZeroPad1d(n, padding=1)
+n1.print_layers()
+shape = n1.outputs.get_shape().as_list()
+if shape[1:] != [102, 1]:
+    raise Exception("shape dont match")
+
+n2 = ZeroPad1d(n, padding=(2, 3))
+n2.print_layers()
+shape = n2.outputs.get_shape().as_list()
+if shape[1:] != [105, 1]:
+    raise Exception("shape dont match")
+
+## 2D
+x = tf.placeholder(tf.float32, (None, 100, 100, 3))
+n = InputLayer(x)
+n1 = ZeroPad2d(n, padding=2)
+n1.print_layers()
+shape = n1.outputs.get_shape().as_list()
+if shape[1:] != [104, 104, 3]:
+    raise Exception("shape dont match")
+
+n2 = ZeroPad2d(n, padding=(2, 3))
+n2.print_layers()
+shape = n2.outputs.get_shape().as_list()
+if shape[1:] != [104, 106, 3]:
+    raise Exception("shape dont match")
+
+n3 = ZeroPad2d(n, padding=((3, 3), (4, 4)))
+n3.print_layers()
+shape = n3.outputs.get_shape().as_list()
+if shape[1:] != [106, 108, 3]:
+    raise Exception("shape dont match")
+
+## 3D
+x = tf.placeholder(tf.float32, (None, 100, 100, 100, 3))
+n = InputLayer(x)
+n1 = ZeroPad3d(n, padding=2)
+n1.print_layers()
+shape = n1.outputs.get_shape().as_list()
+if shape[1:] != [104, 104, 104, 3]:
+    raise Exception("shape dont match")
+
+n2 = ZeroPad3d(n, padding=(2, 3, 4))
+n2.print_layers()
+shape = n2.outputs.get_shape().as_list()
+if shape[1:] != [104, 106, 108, 3]:
+    raise Exception("shape dont match")
+
+n3 = ZeroPad3d(n, padding=((3, 3), (4, 4), (5, 5)))
+n3.print_layers()
+shape = n3.outputs.get_shape().as_list()
+if shape[1:] != [106, 108, 110, 3]:
+    raise Exception("shape dont match")
