8b96b3248eb260a6947066e478c504816feda71f
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2018-04-16 15:48:30 +0200

    PyDoc && YAPF tests refactored

--- tests/test_pydocstyle.py
+++ tests/test_pydocstyle.py
@@ -1,21 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+try:
+    import tests.testing as testing
+except:
+    import testing
+
 from pydocstyle.checker import check
 from pydocstyle.checker import violations
 
-import testing
-
 registry = violations.ErrorRegistry
-
-_disabled_checks = [
-    'D202',  #  No blank lines allowed after function docstring
-    'D205',  #  1 blank line required between summary line and description
-]
-
-
-def check_all_files():
-    for filename in testing.list_all_py_files():
-        for err in check([filename]):
-            if not err.code in _disabled_checks:
-                yield err
 
 
 def lookup_error_params(code):
@@ -25,16 +21,41 @@ def lookup_error_params(code):
                 return error_params
 
 
-violations = list(check_all_files())
+class PyDOC_Style_Test(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
 
-if violations:
-    counts = dict()
-    for e in violations:
-        print(e)
-        counts[e.code] = counts.get(e.code, 0) + 1
+        cls.violations = list()
 
-    for n, code in sorted([(n, code) for code, n in counts.items()], reverse=True):
-        p = lookup_error_params(code)
-        print('%s %8d %s' % (code, n, p.short_desc))
-    print('%s %8d violations' % ('tot', len(violations)))
-    # TODO: exit(1)
+        _disabled_checks = [
+            'D202',  # No blank lines allowed after function docstring
+            'D205',  # 1 blank line required between summary line and description
+        ]
+
+        for filename in testing.list_all_py_files():
+
+            print(filename)
+            for err in check([filename]):
+
+                if not err.code in _disabled_checks:
+                    cls.violations.append(err)
+
+    def test_violations(self):
+        if self.violations:
+
+            counts = dict()
+
+            for err in self.violations:
+                counts[err.code] = counts.get(err.code, 0) + 1
+
+            for n, code in sorted([(n, code) for code, n in counts.items()], reverse=True):
+                p = lookup_error_params(code)
+                print('%s %8d %s' % (code, n, p.short_desc))
+
+            print()
+
+            #raise Exception('PyDoc Coding Style: %d violations have been found' % ( len(self.violations)))  ## TODO: Correct these errors to allow Exception
+
+
+if __name__ == '__main__':
+    unittest.main()
