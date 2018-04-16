bde0e679e6447c4ec0a752048a039d2586eee81e
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2018-04-16 17:36:10 +0200

    Test VGG Models refactored

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,62 +1,93 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import unittest
 
 import tensorflow as tf
 import tensorlayer as tl
 
-with tf.Graph().as_default() as graph:
-    # - Classify ImageNet classes with VGG16, see `tutorial_models_vgg16.py <https://github.com/tensorlayer/tensorlayer/blob/master/example/tutorial_models_vgg16.py>__`
-    x = tf.placeholder(tf.float32, [None, 224, 224, 3])
-    # get the whole model
-    vgg = tl.models.VGG16(x)
-    # restore pre-trained VGG parameters
-    # sess = tf.InteractiveSession()
-    # vgg.restore_params(sess)
-    # use for inferencing
-    probs = tf.nn.softmax(vgg.outputs)
-    if len(vgg.all_layers) != 22:
-        raise Exception("layers do not match")
 
-    if len(vgg.all_params) != 32:
-        raise Exception("params do not match")
+class Layer_Basic_Test(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
 
-with tf.Graph().as_default() as graph:
-    # - Extract features with VGG16 and Train a classifier with 100 classes
-    x = tf.placeholder(tf.float32, [None, 224, 224, 3])
-    # get VGG without the last layer
-    vgg = tl.models.VGG16(x, end_with='fc2_relu')
+        with tf.Graph().as_default():
+            # - Classify ImageNet classes with VGG16, see `tutorial_models_vgg16.py <https://github.com/tensorlayer/tensorlayer/blob/master/example/tutorial_models_vgg16.py>__`
+            x = tf.placeholder(tf.float32, [None, 224, 224, 3])
+            # get the whole model
+            vgg1 = tl.models.VGG16(x)
+            # restore pre-trained VGG parameters
+            # sess = tf.InteractiveSession()
+            # vgg.restore_params(sess)
+            # use for inferencing
+            # probs = tf.nn.softmax(vgg1.outputs)
 
-    if len(vgg.all_layers) != 21:
-        raise Exception("layers do not match")
+            cls.vgg1_layers = vgg1.all_layers
+            cls.vgg1_params = vgg1.all_params
 
-    if len(vgg.all_params) != 30:
-        raise Exception("params do not match")
+        with tf.Graph().as_default():
+            # - Extract features with VGG16 and Train a classifier with 100 classes
+            x = tf.placeholder(tf.float32, [None, 224, 224, 3])
+            # get VGG without the last layer
+            vgg2 = tl.models.VGG16(x, end_with='fc2_relu')
 
-    # add one more layer
-    net = tl.layers.DenseLayer(vgg, n_units=100, name='out')
-    # initialize all parameters
-    # sess = tf.InteractiveSession()
-    # tl.layers.initialize_global_variables(sess)
-    # restore pre-trained VGG parameters
-    # vgg.restore_params(sess)
-    # train your own classifier (only update the last layer)
-    train_params = tl.layers.get_variables_with_name('out')
-    if len(train_params) != 2:
-        raise Exception("params do not match")
+            cls.vgg2_layers = vgg2.all_layers
+            cls.vgg2_params = vgg2.all_params
 
-with tf.Graph().as_default() as graph:
-    # - Reuse model
-    x1 = tf.placeholder(tf.float32, [None, 224, 224, 3])
-    x2 = tf.placeholder(tf.float32, [None, 224, 224, 3])
-    # get VGG without the last layer
-    vgg1 = tl.models.VGG16(x1, end_with='fc2_relu')
-    # reuse the parameters of vgg1 with different input
-    vgg2 = tl.models.VGG16(x2, end_with='fc2_relu', reuse=True)
-    # restore pre-trained VGG parameters (as they share parameters, we don’t need to restore vgg2)
-    # sess = tf.InteractiveSession()
-    # vgg1.restore_params(sess)
+            # add one more layer
+            _ = tl.layers.DenseLayer(vgg2, n_units=100, name='out')
+            # initialize all parameters
+            # sess = tf.InteractiveSession()
+            # tl.layers.initialize_global_variables(sess)
+            # restore pre-trained VGG parameters
+            # vgg.restore_params(sess)
+            # train your own classifier (only update the last layer)
 
-    if len(vgg1.all_layers) != 21:
-        raise Exception("layers do not match")
+            cls.vgg2_train_params = tl.layers.get_variables_with_name('out')
 
-    if len(vgg1.all_params) != 30:
-        raise Exception("params do not match")
+        with tf.Graph().as_default():
+            # - Reuse model
+            x1 = tf.placeholder(tf.float32, [None, 224, 224, 3])
+            x2 = tf.placeholder(tf.float32, [None, 224, 224, 3])
+            # get VGG without the last layer
+            vgg3 = tl.models.VGG16(x1, end_with='fc2_relu')
+            # reuse the parameters of vgg1 with different input
+            _ = tl.models.VGG16(x2, end_with='fc2_relu', reuse=True)
+            # restore pre-trained VGG parameters (as they share parameters, we don’t need to restore vgg2)
+            # sess = tf.InteractiveSession()
+            # vgg1.restore_params(sess)
+
+            cls.vgg3_layers = vgg3.all_layers
+            cls.vgg3_params = vgg3.all_params
+
+    @classmethod
+    def tearDownClass(cls):
+        tf.reset_default_graph()
+
+    def test_vgg1_layers(self):
+        assert (len(self.vgg1_layers) == 22)
+
+    def test_vgg2_layers(self):
+        assert (len(self.vgg2_layers) == 21)
+
+    def test_vgg3_layers(self):
+        assert (len(self.vgg3_layers) == 21)
+
+    def test_vgg1_params(self):
+        assert (len(self.vgg1_params) == 32)
+
+    def test_vgg2_params(self):
+        assert (len(self.vgg2_params) == 30)
+
+    def test_vgg3_params(self):
+        assert (len(self.vgg3_params) == 30)
+
+    def test_vgg2_train_params(self):
+        assert (len(self.vgg2_train_params) == 2)
+
+
+if __name__ == '__main__':
+
+    # tf.logging.set_verbosity(tf.logging.INFO)
+    tf.logging.set_verbosity(tf.logging.DEBUG)
+
+    unittest.main()
