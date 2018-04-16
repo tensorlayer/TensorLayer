73e479ac44853e6f1ec9913bee8af67d5e9022f4
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2018-04-16 15:07:32 +0200

    test layers: extend & normalization refactored

--- tests/test_layers_normalization.py
+++ tests/test_layers_normalization.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+
 import tensorflow as tf
 import tensorlayer as tl
 
@@ -6,26 +10,55 @@ def model(x, is_train=True, reuse=False):
     with tf.variable_scope("model", reuse=reuse):
         n = tl.layers.InputLayer(x, name='in')
         n = tl.layers.Conv2d(n, n_filter=80, name='conv2d_1')
-        n = tl.layers.BatchNormLayer(n, name='norm_batch')
+        n = tl.layers.BatchNormLayer(n, is_train=is_train, name='norm_batch')
         n = tl.layers.Conv2d(n, n_filter=80, name='conv2d_2')
         n = tl.layers.LocalResponseNormLayer(n, name='norm_local')
-        n = tl.layers.LayerNormLayer(n, name='norm_layer')
+        n = tl.layers.LayerNormLayer(n, reuse=reuse, name='norm_layer')
         n = tl.layers.InstanceNormLayer(n, name='norm_instance')
     return n
 
 
-x = tf.placeholder(tf.float32, [None, 100, 100, 3])
-net = model(x, True, False)
-_ = model(x, False, True)
+class Layer_Basic_Test(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
 
-net.print_layers()
-net.print_params(False)
+        x = tf.placeholder(tf.float32, [None, 100, 100, 3])
 
-if len(net.all_layers) != 6:
-    raise Exception("layers do not match")
+        net_train = model(x, is_train=True, reuse=False)
+        net_eval = model(x, is_train=False, reuse=True)
 
-if len(net.all_params) != 12:
-    raise Exception("params do not match")
+        net_train.print_layers()
+        net_train.print_params(False)
 
-if net.count_params() != 60560:
-    raise Exception("params do not match")
+        cls.data = dict()
+        cls.data["train_network"] = dict()
+        cls.data["eval_network"] = dict()
+
+        cls.data["train_network"]["layers"] = net_train.all_layers
+        cls.data["eval_network"]["layers"] = net_eval.all_layers
+
+        cls.data["train_network"]["params"] = net_train.all_params
+
+        cls.data["train_network"]["n_params"] = net_train.count_params()
+
+    @classmethod
+    def tearDownClass(cls):
+        tf.reset_default_graph()
+
+    def test_all_layers(self):
+        assert (len(self.data["train_network"]["layers"]) == 6)
+        assert (len(self.data["eval_network"]["layers"]) == 6)
+
+    def test_all_params(self):
+        assert (len(self.data["train_network"]["params"]) == 12)
+
+    def test_n_params(self):
+        assert (self.data["train_network"]["n_params"] == 60560)
+
+
+if __name__ == '__main__':
+
+    # tf.logging.set_verbosity(tf.logging.INFO)
+    tf.logging.set_verbosity(tf.logging.DEBUG)
+
+    unittest.main()
