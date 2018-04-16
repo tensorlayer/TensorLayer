f8aced6d4c994cdd72e19fb25a834ab1cf98558f
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2018-04-16 14:21:23 +0200

    Error fix with Licence in setup.py

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ setup(
         'Topic :: Utilities',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
