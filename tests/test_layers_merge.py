a22482f84ea30b4ef49b598f823e432bf13ed64c
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2018-04-16 14:45:29 +0200

    test layers: merge & stack refactored

--- tests/test_layers_merge.py
+++ tests/test_layers_merge.py
@@ -1,79 +1,110 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+
 import tensorflow as tf
 import tensorlayer as tl
 
-sess = tf.InteractiveSession()
 
-## vector
-x = tf.placeholder(tf.float32, shape=[None, 784])
-inputs = tl.layers.InputLayer(x, name='input_layer')
-net1 = tl.layers.DenseLayer(inputs, n_units=100, act=tf.nn.relu, name='relu1_1')
-net2 = tl.layers.DenseLayer(inputs, n_units=100, act=tf.nn.relu, name='relu2_1')
-net = tl.layers.ConcatLayer([net1, net2], concat_dim=1, name='concat_layer')
+class Layer_Basic_Test(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
 
-net.print_params(False)
-net.print_layers()
+        cls.data = dict()
 
-if len(net.all_layers) != 3:
-    raise Exception("layers do not match")
+        ##############
+        #   vector   #
+        ##############
 
-if len(net.all_params) != 4:
-    raise Exception("params do not match")
+        x = tf.placeholder(tf.float32, shape=[None, 784])
+        inputs = tl.layers.InputLayer(x, name='input_layer')
 
-if net.count_params() != 157000:
-    raise Exception("params do not match")
+        net_v1_1 = tl.layers.DenseLayer(inputs, n_units=100, act=tf.nn.relu, name='relu1_1')
+        net_v1_2 = tl.layers.DenseLayer(inputs, n_units=100, act=tf.nn.relu, name='relu2_1')
+        net_v1 = tl.layers.ConcatLayer([net_v1_1, net_v1_2], concat_dim=1, name='concat_layer')
 
-net_0 = tl.layers.DenseLayer(inputs, n_units=100, act=tf.nn.relu, name='net_0')
-net_1 = tl.layers.DenseLayer(inputs, n_units=100, act=tf.nn.relu, name='net_1')
-net = tl.layers.ElementwiseLayer([net_0, net_1], combine_fn=tf.minimum, name='minimum')
+        net_v1.print_params(False)
+        net_v1.print_layers()
 
-net.print_params(False)
-net.print_layers()
+        cls.data["net_vector1"] = dict()
+        cls.data["net_vector1"]["layers"] = net_v1.all_layers
+        cls.data["net_vector1"]["params"] = net_v1.all_params
+        cls.data["net_vector1"]["n_params"] = net_v1.count_params()
 
-if len(net.all_layers) != 3:
-    raise Exception("layers do not match")
+        net_v2_1 = tl.layers.DenseLayer(inputs, n_units=100, act=tf.nn.relu, name='net_0')
+        net_v2_2 = tl.layers.DenseLayer(inputs, n_units=100, act=tf.nn.relu, name='net_1')
+        net_v2 = tl.layers.ElementwiseLayer([net_v2_1, net_v2_2], combine_fn=tf.minimum, name='minimum')
 
-if len(net.all_params) != 4:
-    raise Exception("params do not match")
+        net_v2.print_params(False)
+        net_v2.print_layers()
 
-if net.count_params() != 157000:
-    raise Exception("params do not match")
+        cls.data["net_vector2"] = dict()
+        cls.data["net_vector2"]["layers"] = net_v2.all_layers
+        cls.data["net_vector2"]["params"] = net_v2.all_params
+        cls.data["net_vector2"]["n_params"] = net_v2.count_params()
 
-## image
-x = tf.placeholder(tf.float32, shape=[None, 100, 100, 3])
-inputs = tl.layers.InputLayer(x, name='input')
-net1 = tl.layers.Conv2d(inputs, n_filter=32, filter_size=(3, 3), strides=(2, 2), act=tf.nn.relu, name='c1')
-net2 = tl.layers.Conv2d(inputs, n_filter=32, filter_size=(3, 3), strides=(2, 2), act=tf.nn.relu, name='c2')
-net = tl.layers.ConcatLayer([net1, net2], concat_dim=-1, name='concat')
 
-net.print_params(False)
-net.print_layers()
+        #############
+        #   Image   #
+        #############
 
-shape = net.outputs.get_shape().as_list()
-if shape[1:] != [50, 50, 64]:
-    raise Exception("shape do not match")
+        x = tf.placeholder(tf.float32, shape=[None, 100, 100, 3])
+        inputs = tl.layers.InputLayer(x, name='input')
 
-if len(net.all_layers) != 3:
-    raise Exception("layers do not match")
+        net_im1_1 = tl.layers.Conv2d(inputs, n_filter=32, filter_size=(3, 3), strides=(2, 2), act=tf.nn.relu, name='c1')
+        net_im1_2 = tl.layers.Conv2d(inputs, n_filter=32, filter_size=(3, 3), strides=(2, 2), act=tf.nn.relu, name='c2')
+        net_im1 = tl.layers.ConcatLayer([net_im1_1, net_im1_2], concat_dim=-1, name='concat')
 
-if len(net.all_params) != 4:
-    raise Exception("params do not match")
+        net_im1.print_params(False)
+        net_im1.print_layers()
 
-if net.count_params() != 1792:
-    raise Exception("params do not match")
+        cls.data["net_image1"] = dict()
+        cls.data["net_image1"]["shape"] = net_im1.outputs.get_shape().as_list()
+        cls.data["net_image1"]["layers"] = net_im1.all_layers
+        cls.data["net_image1"]["params"] = net_im1.all_params
+        cls.data["net_image1"]["n_params"] = net_im1.count_params()
 
-net = tl.layers.ElementwiseLayer([net1, net2], combine_fn=tf.minimum, name='minimum2')
-net.print_params(False)
-net.print_layers()
+        net_im2 = tl.layers.ElementwiseLayer([net_im1_1, net_im1_2], combine_fn=tf.minimum, name='minimum2')
 
-shape = net.outputs.get_shape().as_list()
-if shape[1:] != [50, 50, 32]:
-    raise Exception("shape do not match")
+        net_im2.print_params(False)
+        net_im2.print_layers()
 
-if len(net.all_layers) != 3:
-    raise Exception("layers do not match")
+        cls.data["net_image2"] = dict()
+        cls.data["net_image2"]["shape"] = net_im2.outputs.get_shape().as_list()
+        cls.data["net_image2"]["layers"] = net_im2.all_layers
+        cls.data["net_image2"]["params"] = net_im2.all_params
+        cls.data["net_image2"]["n_params"] = net_im2.count_params()
 
-if len(net.all_params) != 4:
-    raise Exception("params do not match")
+    @classmethod
+    def tearDownClass(cls):
+        tf.reset_default_graph()
 
-if net.count_params() != 1792:
-    raise Exception("params do not match")
+    def test_net_vector1(self):
+        assert (len(self.data["net_vector1"]["layers"]) == 3)
+        assert (len(self.data["net_vector1"]["params"]) == 4)
+        assert (self.data["net_vector1"]["n_params"] == 157000)
+
+    def test_net_vector2(self):
+        assert (len(self.data["net_vector2"]["layers"]) == 3)
+        assert (len(self.data["net_vector2"]["params"]) == 4)
+        assert (self.data["net_vector2"]["n_params"] == 157000)
+
+    def test_net_image1(self):
+        assert (self.data["net_image1"]["shape"][1:] == [50, 50, 64])
+        assert (len(self.data["net_image1"]["layers"]) == 3)
+        assert (len(self.data["net_image1"]["params"]) == 4)
+        assert (self.data["net_image1"]["n_params"] == 1792)
+
+    def test_net_image2(self):
+        assert (self.data["net_image2"]["shape"][1:] == [50, 50, 32])
+        assert (len(self.data["net_image2"]["layers"]) == 3)
+        assert (len(self.data["net_image2"]["params"]) == 4)
+        assert (self.data["net_image2"]["n_params"] == 1792)
+
+
+if __name__ == '__main__':
+
+    # tf.logging.set_verbosity(tf.logging.INFO)
+    tf.logging.set_verbosity(tf.logging.DEBUG)
+
+    unittest.main()
--- tests/test_layers_stack.py
+++ tests/test_layers_stack.py
@@ -1,44 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+
 import tensorflow as tf
 import tensorlayer as tl
 
-x = tf.placeholder(tf.float32, shape=[None, 30])
-net = tl.layers.InputLayer(x, name='input')
-net1 = tl.layers.DenseLayer(net, n_units=10, name='dense1')
-net2 = tl.layers.DenseLayer(net, n_units=10, name='dense2')
-net3 = tl.layers.DenseLayer(net, n_units=10, name='dense3')
-net = tl.layers.StackLayer([net1, net2, net3], axis=1, name='stack')
 
-net.print_layers()
-net.print_params(False)
+class Layer_Basic_Test(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
 
-shape = net.outputs.get_shape().as_list()
-if shape[-1] != 10:
-    raise Exception("shape do not match")
+        x = tf.placeholder(tf.float32, shape=[None, 30])
+        net = tl.layers.InputLayer(x, name='input')
+        net1 = tl.layers.DenseLayer(net, n_units=10, name='dense1')
+        net2 = tl.layers.DenseLayer(net, n_units=10, name='dense2')
+        net3 = tl.layers.DenseLayer(net, n_units=10, name='dense3')
+        net = tl.layers.StackLayer([net1, net2, net3], axis=1, name='stack')
 
-if len(net.all_layers) != 4:
-    raise Exception("layers do not match")
+        net.print_layers()
+        net.print_params(False)
 
-if len(net.all_params) != 6:
-    raise Exception("params do not match")
+        cls.net_shape = net.outputs.get_shape().as_list()
+        cls.layers = net.all_layers
+        cls.params = net.all_params
+        cls.n_params = net.count_params()
 
-if net.count_params() != 930:
-    raise Exception("params do not match")
+        cls.net = tl.layers.UnStackLayer(net, axis=1, name='unstack')
 
-net = tl.layers.UnStackLayer(net, axis=1, name='unstack')
-for n in net:
-    print(n, n.outputs)
-    shape = n.outputs.get_shape().as_list()
-    if shape[-1] != 10:
-        raise Exception("shape do not match")
+    @classmethod
+    def tearDownClass(cls):
+        tf.reset_default_graph()
 
-    # n.print_layers()
-    # n.print_params(False)
+    def test_net_shape(self):
+        assert (self.net_shape[-1] == 10)
 
-    if len(n.all_layers) != 4:
-        raise Exception("layers do not match")
+    def test_layers(self):
+        assert (len(self.layers) == 4)
 
-    if len(n.all_params) != 6:
-        raise Exception("params do not match")
+    def test_params(self):
+        assert (len(self.params) == 6)
+        assert (self.n_params == 930)
 
-    if n.count_params() != 930:
-        raise Exception("params do not match")
+    def test_unstack(self):
+
+        for n in self.net:
+            shape = n.outputs.get_shape().as_list()
+
+            assert(shape[-1] == 10)
+            assert(len(n.all_layers) == 4)
+            assert(len(n.all_params) == 6)
+            assert(n.count_params() == 930)
+
+
+if __name__ == '__main__':
+
+    # tf.logging.set_verbosity(tf.logging.INFO)
+    tf.logging.set_verbosity(tf.logging.DEBUG)
+
+    unittest.main()
