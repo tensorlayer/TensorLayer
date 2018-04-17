6f539ad2324ddccc18b6ac39e4a2f4aeb12173a2
Jonathan DEKHTIAR <DEKHTIARJonathan@users.noreply.github.com>
2018-04-17 09:50:42 +0200

    Test Running with Unittest and PyTest Suite (#494)
    * Test Refactoring - Layer Basic & Layer Convolution
    
    * pytest configuration added
    
    * test layers: merge & stack refactored
    
    * test layers: extend & normalization refactored
    
    * yapf cleaning
    
    * PyDoc && YAPF tests refactored
    
    * Test Layer Time Distributed Refactored + Unittest Custom Class with AssertNotRaises method added
    
    * Test Layer - Special Activation Refactored
    
    * Test Layers: Flow Control & Super Resolution Refactored
    
    * Test Reuse MLP  Refactored
    
    * Test Layer - Spatial Transformer Refactored
    
    * Test Layer - Padding refactored
    
    * Test Layer - Shape Refactored
    
    * Test Layer - Importer Refactored
    
    * Test VGG Models refactored
    
    * Test Models - Reuse VGG changed to unittest
    
    * Test MNIST Model Refactored
    
    * Test Layer - Pooling Refactored
    
    * Test Layer - Core Refactored
    
    * Test Layer - Recurrent Refactor
    
    * TestCases renamed
    
    * Travis Config Updated
    
    * Travis Config Updated
    
    * Travis Config Updated
    
    * Travis Deploy Script Updated
    
    * Unnecessary Code removed - Tests cleaned
    
    * Test Refactored - Codacy Errors: unused variables Removed
    
    * Test Refactored - Codacy Errors: unused variables Removed
    
    * Test Refactored - Codacy Errors: exception type added => ImportError

--- tests/test_layers_time_distributed.py
+++ tests/test_layers_time_distributed.py
@@ -1,31 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+
+try:
+    from tests.unittests_helper import CustomTestCase
+except ImportError:
+    from unittests_helper import CustomTestCase
+
 import tensorflow as tf
-from tensorlayer.layers import InputLayer, TimeDistributedLayer, DenseLayer
-
-sess = tf.InteractiveSession()
-batch_size = 32
-timestep = 20
-input_dim = 100
-
-## no reuse
-x = tf.placeholder(dtype=tf.float32, shape=[batch_size, timestep, input_dim], name="encode_seqs")
-net = InputLayer(x, name='input')
-net = TimeDistributedLayer(net, layer_class=DenseLayer, args={'n_units': 50, 'name': 'dense'}, name='time_dense')
-
-if net.outputs.get_shape().as_list() != [32, 20, 50]:
-    raise Exception("shape do not match")
-# ... (32, 20, 50)
-net.print_params(False)
-if net.count_params() != 5050:
-    raise Exception("params do not match")
+import tensorlayer as tl
 
 
-## reuse
-def model(x, is_train=True, reuse=False):
-    with tf.variable_scope("model", reuse=reuse):
-        net = InputLayer(x, name='input')
-        net = TimeDistributedLayer(net, layer_class=DenseLayer, args={'n_units': 50, 'name': 'dense'}, name='time_dense')
+def model(x, is_train=True, reuse=False, name_scope="env1"):
+    with tf.variable_scope(name_scope, reuse=reuse):
+        net = tl.layers.InputLayer(x, name='input')
+        net = tl.layers.TimeDistributedLayer(net, layer_class=tl.layers.DenseLayer, args={'n_units': 50, 'name': 'dense'}, name='time_dense')
     return net
 
 
-net_train = model(x, is_train=True, reuse=False)
-net_test = model(x, is_train=False, reuse=True)
+class Layer_Time_Distributed_Test(CustomTestCase):
+    @classmethod
+    def setUpClass(cls):
+
+        batch_size = 32
+        timestep = 20
+        input_dim = 100
+
+        cls.x = tf.placeholder(dtype=tf.float32, shape=[batch_size, timestep, input_dim], name="encode_seqs")
+        net = model(cls.x, is_train=True, reuse=False)
+
+        cls.net_shape = net.outputs.get_shape().as_list()
+        cls.n_params = net.count_params()
+        net.print_params(False)
+
+    @classmethod
+    def tearDownClass(cls):
+        tf.reset_default_graph()
+
+    def test_net_shape(self):
+        self.assertEqual(self.net_shape, [32, 20, 50])
+
+    def test_net_n_params(self):
+        self.assertEqual(self.n_params, 5050)
+
+    def test_reuse(self):
+
+        with self.assertNotRaises(Exception):
+            model(self.x, is_train=True, reuse=False, name_scope="env2")
+            model(self.x, is_train=False, reuse=True, name_scope="env2")
+
+        with self.assertRaises(Exception):
+            model(self.x, is_train=True, reuse=False)  # Already defined model with the same var_scope
+
+
+if __name__ == '__main__':
+
+    # tf.logging.set_verbosity(tf.logging.INFO)
+    tf.logging.set_verbosity(tf.logging.DEBUG)
+
+    unittest.main()
--- tests/test_yapf_format.py
+++ tests/test_yapf_format.py
@@ -1,30 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import sys
-import testing
+import unittest
+
+try:
+    import tests.testing as testing
+except ImportError:
+    import testing
+
 from yapf.yapflib.yapf_api import FormatCode
 
 
 def _read_utf_8_file(filename):
-    if sys.version_info.major == 2:
-        return unicode(open(filename, 'rb').read(), 'utf-8')
+    if sys.version_info.major == 2:  ## Python 2 specific
+        with open(filename, 'rb') as f:
+            return unicode(f.read(), 'utf-8')
     else:
-        return open(filename, encoding='utf-8').read()
+        with open(filename, encoding='utf-8') as f:
+            return f.read()
 
 
-def check_all_files():
-    for filename in testing.list_all_py_files():
-        print(filename)
-        code = _read_utf_8_file(filename)
-        # https://pypi.python.org/pypi/yapf/0.20.2#example-as-a-module
-        diff, changed = FormatCode(code, filename=filename, style_config='.style.yapf', print_diff=True)
-        if changed:
-            print(diff)
-            yield filename
+class YAPF_Style_Test(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+
+        cls.files_badly_formated = list()
+
+        for filename in testing.list_all_py_files():
+
+            print(filename)
+            code = _read_utf_8_file(filename)
+
+            # https://pypi.python.org/pypi/yapf/0.20.2#example-as-a-module
+            diff, changed = FormatCode(code, filename=filename, style_config='.style.yapf', print_diff=True)
+
+            if changed:
+                print(diff)
+                cls.files_badly_formated.append(filename)
+
+    def test_unformated_files(self):
+        if self.files_badly_formated:
+            print()
+
+            for filename in self.files_badly_formated:
+                print('yapf -i %s' % filename)
+
+            raise Exception("Bad Coding Style: %d files need to be formatted, run the following commands to fix" % len(self.files_badly_formated))
 
 
-unformatted = list(check_all_files())
-
-if unformatted:
-    print('%d files need to be formatted, run the following commands to fix' % len(unformatted))
-    for filename in unformatted:
-        print('yapf -i %s' % filename)
-    exit(1)
+if __name__ == '__main__':
+    unittest.main()
