03b3a979382dd2bcb240253f3a7d25b3a7512d90
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2018-05-06 15:37:33 +0200

    Error Fix

--- tensorlayer/__init__.py
+++ tensorlayer/__init__.py
@@ -3,8 +3,6 @@
 """Deep learning and Reinforcement learning library for Researchers and Engineers"""
 from __future__ import absolute_import
 
-import pkg_resources
-installed_packages = [d for d in pkg_resources.working_set]
 
 try:
     import tensorflow
@@ -30,10 +28,15 @@ try:
     global_dict = {}
 
 except Exception as e:
-    if 'tensorlayer' in package.project_name and 'site-packages' in package.location:
-        raise ImportError("__init__.py : Could not import TensorLayer.\nError: {}".format(e))
-    else:
-        pass
+
+	import pkg_resources
+	installed_packages = [d for d in pkg_resources.working_set]
+
+	for package in installed_packages:
+		if 'tensorlayer' in package.project_name and 'site-packages' in package.location:
+			raise ImportError("__init__.py : Could not import TensorLayer.\nError: {}".format(e))
+    
+    pass
 
 # Use the following formating: (major, minor, patch, prerelease)
 VERSION = (1, 8, 5, 'rc2')
