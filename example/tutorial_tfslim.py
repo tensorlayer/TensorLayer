d9169b90c9a0e5863ebd2f286d37cdc6572b5614
2Wins <twinkyu@live.com>
2018-05-10 16:22:45 +0900

    Update tutorial_tfslim.py

--- example/tutorial_tfslim.py
+++ example/tutorial_tfslim.py
@@ -18,17 +18,18 @@ is_training = tf.placeholder(tf.bool)
 
 
 def slim_block(x):
-    x = slim.dropout(x, 0.8, is_training=is_training)
-    x = slim.fully_connected(x, 800, activation_fn=tf.nn.relu)
-    x = slim.dropout(x, 0.5, is_training=is_training)
-    x = slim.fully_connected(x, 800, activation_fn=tf.nn.relu)
-    x = slim.dropout(x, 0.5, is_training=is_training)
-    logits = slim.fully_connected(x, 10, activation_fn=tf.identity)
-    return logits
+    with tf.variable_scope('tf_slim'):
+        x = slim.dropout(x, 0.8, is_training=is_training)
+        x = slim.fully_connected(x, 800, activation_fn=tf.nn.relu)
+        x = slim.dropout(x, 0.5, is_training=is_training)
+        x = slim.fully_connected(x, 800, activation_fn=tf.nn.relu)
+        x = slim.dropout(x, 0.5, is_training=is_training)
+        logits = slim.fully_connected(x, 10, activation_fn=tf.identity)
+    return logits, {}
 
 
 network = InputLayer(x, name='input')
-network = LambdaLayer(network, fn=slim_block, name='tf_slim')
+network = SlimNetsLayer(network, slim_layer=slim_block, name='tf_slim')
 
 y = network.outputs
 network.print_params(False)
