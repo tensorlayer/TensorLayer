e305101cd54c57c8eee8aa42f10d5cea804820af
Jonathan DEKHTIAR <contact@jonathandekhtiar.eu>
2018-05-30 20:32:54 +0200

    Logging fix (#661)
    * `tf.logging` replaced by: `tl.logging`
    
    * Changelog updated

--- tests/test_layers_convolution.py
+++ tests/test_layers_convolution.py
@@ -244,7 +244,7 @@ class Layer_DeformableConvolution_Test(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    # tf.logging.set_verbosity(tf.logging.INFO)
-    tf.logging.set_verbosity(tf.logging.DEBUG)
+    # tl.logging.set_verbosity(tl.logging.INFO)
+    tl.logging.set_verbosity(tl.logging.DEBUG)
 
     unittest.main()
