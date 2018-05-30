ccfce9f4fca51b97aa58d798a0d1a39946d93db0
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2018-05-30 11:38:31 +0200

    _compute_threshold deprecation warning fixed

--- tensorlayer/layers/binary.py
+++ tensorlayer/layers/binary.py
@@ -65,7 +65,7 @@ def _compute_threshold(x):
     ref: https://github.com/XJTUWYD/TWN
     Computing the threshold.
     """
-    x_sum = tf.reduce_sum(tf.abs(x), reduction_indices=None, keep_dims=False, name=None)
+    x_sum = tf.reduce_sum(tf.abs(x), reduction_indices=None, keepdims=False, name=None)
     threshold = tf.div(x_sum, tf.cast(tf.size(x), tf.float32), name=None)
     threshold = tf.multiply(0.7, threshold, name=None)
     return threshold
@@ -548,6 +548,7 @@ class TernaryConv2d(Layer):
                         data_format=data_format
                     ) + b
                 )
+
             else:
                 self.outputs = act(
                     tf.nn.conv2d(
@@ -557,6 +558,7 @@ class TernaryConv2d(Layer):
                 )
 
         self.all_layers.append(self.outputs)
+
         if b_init:
             self.all_params.extend([W, b])
         else:
