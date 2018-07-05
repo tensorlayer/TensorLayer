6b96299f21aaf56e9aa3ba1d828d17316e2775b6
Hao <dhsig552@163.com>
2018-07-05 16:00:09 +0100

    yapf

--- example/tutorial_mnist_distributed_trainer.py
+++ example/tutorial_mnist_distributed_trainer.py
@@ -52,11 +52,8 @@ if __name__ == '__main__':
     training_dataset = make_dataset(X_train, y_train)
     # validation_dataset = make_dataset(X_val, y_val)
     trainer = tl.distributed.Trainer(
-        build_training_func=build_train,
-        training_dataset=training_dataset,
-        batch_size=32,
-        optimizer=tf.train.RMSPropOptimizer,
-        optimizer_args={'learning_rate': 0.001}
+        build_training_func=build_train, training_dataset=training_dataset, batch_size=32,
+        optimizer=tf.train.RMSPropOptimizer, optimizer_args={'learning_rate': 0.001}
         # validation_dataset=validation_dataset, build_validation_func=build_validation
     )
 
