2bc6094b613409588442e8e6d114b6bab68bf4aa
Luo Mai <luo.mai.cs@gmail.com>
2018-07-09 11:30:02 +0100

    check in a script to help install cuda 9 and cuDNN 7

---
+++ scripts/download_and_install_cuda9_ubuntu.sh
@@ -0,0 +1,34 @@
+# This script downloads and installs CUDA 9.0 and cuDNN 7.0 on a Ubuntu machine
+# Reference: https://yangcha.github.io/CUDA90/
+
+echo `You may need to uninstall old version of CUDA by uncommenting part of code in this script`
+# (Optional) Uninstall old version CUDA Toolkit by uncommenting the following three lines:
+# sudo apt-get purge cuda
+# sudo apt-get purge libcudnn6
+# sudo apt-get purge libcudnn6-dev
+
+echo `Downloading all packages....`
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7_7.0.5.15-1+cuda9.0_amd64.deb
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7-dev_7.0.5.15-1+cuda9.0_amd64.deb
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libnccl2_2.1.4-1+cuda9.0_amd64.deb
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libnccl-dev_2.1.4-1+cuda9.0_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+sudo dpkg -i libcudnn7_7.0.5.15-1+cuda9.0_amd64.deb
+sudo dpkg -i libcudnn7-dev_7.0.5.15-1+cuda9.0_amd64.deb
+sudo dpkg -i libnccl2_2.1.4-1+cuda9.0_amd64.deb
+sudo dpkg -i libnccl-dev_2.1.4-1+cuda9.0_amd64.deb
+
+echo `Installing all packages....`
+sudo apt-get update
+sudo apt-get install cuda=9.0.176-1
+sudo apt-get install libcudnn7-dev
+sudo apt-get install libnccl-dev
+
+echo `Setting up the development environment by modifying the PATH and LD_LIBRARY_PATH variables`
+export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+
+echo 'You can add them to the end of .bashrc file by adding the following two lines'
+echo 'PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}'
+echo 'LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}'
