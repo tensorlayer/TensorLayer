2cb13b9f541468562e41e63b01d0497c2c425d09
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2018-07-31 12:57:24 +0200

    Deformable Conv Refactored

--- tensorlayer/layers/convolution/deformable_conv.py
+++ tensorlayer/layers/convolution/deformable_conv.py
@@ -4,11 +4,11 @@
 import tensorflow as tf
 
 from tensorlayer.layers.core import Layer
-from tensorlayer.layers.core import LayersConfig
 
 from tensorlayer import logging
 
 from tensorlayer.decorators import deprecated_alias
+from tensorlayer.decorators import force_return_self
 from tensorlayer.decorators import private_method
 
 __all__ = [
@@ -67,91 +67,153 @@ class DeformableConv2d(Layer):
     @deprecated_alias(layer='prev_layer', end_support_version=1.9)  # TODO remove this line for the 1.9 release
     def __init__(
             self,
-            prev_layer,
+            prev_layer=None,
             offset_layer=None,
-            # shape=(3, 3, 1, 100),
             n_filter=32,
             filter_size=(3, 3),
-            act=None,
-            name='deformable_conv_2d',
             W_init=tf.truncated_normal_initializer(stddev=0.02),
             b_init=tf.constant_initializer(value=0.0),
             W_init_args=None,
-            b_init_args=None
+            b_init_args=None,
+            act=None,
+            name='deformable_conv_2d',
     ):
 
-        super(DeformableConv2d, self
-             ).__init__(prev_layer=prev_layer, act=act, W_init_args=W_init_args, b_init_args=b_init_args, name=name)
+        self.prev_layer = self._check_inputs(prev_layer, offset_layer)
 
-        logging.info(
-            "DeformableConv2d %s: n_filter: %d, filter_size: %s act: %s" %
-            (self.name, n_filter, str(filter_size), self.act.__name__ if self.act is not None else 'No Activation')
-        )
+        self.n_filter = n_filter
+        self.filter_size = filter_size
+        self.W_init = W_init
+        self.b_init = b_init
+        self.act = act
+        self.name = name
 
-        self.offset_layer = offset_layer
+        super(DeformableConv2d, self).__init__(W_init_args=W_init_args, b_init_args=b_init_args)
+
+    def __str__(self):
+        additional_str = []
 
         try:
-            pre_channel = int(prev_layer.outputs.get_shape()[-1])
-        except Exception:  # if pre_channel is ?, it happens when using Spatial Transformer Net
-            pre_channel = 1
-            logging.info("[warnings] unknow input channels, set to 1")
-        shape = (filter_size[0], filter_size[1], pre_channel, n_filter)
+            additional_str.append("n_filter: %d" % self.n_filter)
+        except AttributeError:
+            pass
 
-        with tf.variable_scope(name):
-            offset = self.offset_layer.outputs
+        try:
+            additional_str.append("filter_size: %s" % str(self.filter_size))
+        except AttributeError:
+            pass
 
-            if offset.get_shape()[-1] != 2 * shape[0] * shape[1]:
-                raise AssertionError("offset.get_shape()[-1] is not equal to: %d" % 2 * shape[0] * shape[1])
+        try:
+            additional_str.append("act: %s" % self.act.__name__ if self.act is not None else 'No Activation')
+        except AttributeError:
+            pass
+
+        return self._str(additional_str)
+
+    @private_method
+    def _check_inputs(self, prev_layer, offset_layer):
+
+        if isinstance(prev_layer, tf.layers.Layer):
+
+            if offset_layer is None:
+                raise ValueError("`theta_layer` cannot be set to None")
+
+            else:
+                return [prev_layer, offset_layer]
+
+        elif isinstance(prev_layer, (list, tuple)):
+            return prev_layer
+
+        else:
+            return None
+
+    @force_return_self
+    def __call__(self, prev_layer, offset_layer=None, is_train=True):
+
+        super(DeformableConv2d, self).__call__(self._check_inputs(prev_layer, offset_layer))
+
+        input_layer = self.inputs[0].outputs
+        offset_layer = self.inputs[1].outputs
+
+        try:
+            input_channels = int(input_layer.get_shape()[-1])
+
+        except Exception:  # if input_channels is ?, it happens when using Spatial Transformer Net
+            input_channels = 1
+            logging.warning("unknown input channels, set to 1")
+
+        w_shape = (self.filter_size[0], self.filter_size[1], input_channels, self.n_filter)
+
+        with tf.variable_scope(self.name):
+
+            if offset_layer.get_shape()[-1] != 2 * w_shape[0] * w_shape[1]:
+                raise AssertionError("offset_layer.get_shape()[-1] is not equal to: %d" % (2 * w_shape[0] * w_shape[1]))
 
             # Grid initialisation
             input_h = int(self.inputs.get_shape()[1])
             input_w = int(self.inputs.get_shape()[2])
-            kernel_n = shape[0] * shape[1]
-            initial_offsets = tf.stack(tf.meshgrid(tf.range(shape[0]), tf.range(shape[1]),
-                                                   indexing='ij'))  # initial_offsets --> (kh, kw, 2)
-            initial_offsets = tf.reshape(initial_offsets, (-1, 2))  # initial_offsets --> (n, 2)
-            initial_offsets = tf.expand_dims(initial_offsets, 0)  # initial_offsets --> (1, n, 2)
-            initial_offsets = tf.expand_dims(initial_offsets, 0)  # initial_offsets --> (1, 1, n, 2)
-            initial_offsets = tf.tile(initial_offsets, [input_h, input_w, 1, 1])  # initial_offsets --> (h, w, n, 2)
+
+            kernel_n = w_shape[0] * w_shape[1]
+
+            # initial_offsets --> (kh, kw, 2)
+            initial_offsets = tf.stack(tf.meshgrid(tf.range(w_shape[0]), tf.range(w_shape[1]), indexing='ij'))
+
+            # initial_offsets --> (n, 2)
+            initial_offsets = tf.reshape(initial_offsets, (-1, 2))
+
+            # initial_offsets --> (1, n, 2)
+            initial_offsets = tf.expand_dims(initial_offsets, 0)
+
+            # initial_offsets --> (1, 1, n, 2)
+            initial_offsets = tf.expand_dims(initial_offsets, 0)
+
+            # initial_offsets --> (h, w, n, 2)
+            initial_offsets = tf.tile(initial_offsets, [input_h, input_w, 1, 1])
+
             initial_offsets = tf.cast(initial_offsets, 'float32')
+
             grid = tf.meshgrid(
-                tf.range(-int((shape[0] - 1) / 2.0), int(input_h - int((shape[0] - 1) / 2.0)), 1),
-                tf.range(-int((shape[1] - 1) / 2.0), int(input_w - int((shape[1] - 1) / 2.0)), 1), indexing='ij'
+                tf.range(-int((w_shape[0] - 1) / 2.0), int(input_h - int((w_shape[0] - 1) / 2.0)), 1),
+                tf.range(-int((w_shape[1] - 1) / 2.0), int(input_w - int((w_shape[1] - 1) / 2.0)), 1), indexing='ij'
             )
 
             grid = tf.stack(grid, axis=-1)
-            grid = tf.cast(grid, 'float32')  # grid --> (h, w, 2)
-            grid = tf.expand_dims(grid, 2)  # grid --> (h, w, 1, 2)
-            grid = tf.tile(grid, [1, 1, kernel_n, 1])  # grid --> (h, w, n, 2)
-            grid_offset = grid + initial_offsets  # grid_offset --> (h, w, n, 2)
 
-            input_deform = self._tf_batch_map_offsets(self.inputs, offset, grid_offset)
+            # grid --> (h, w, 2)
+            grid = tf.cast(grid, 'float32')
+
+            # grid --> (h, w, 1, 2)
+            grid = tf.expand_dims(grid, 2)
+
+            # grid --> (h, w, n, 2)
+            grid = tf.tile(grid, [1, 1, kernel_n, 1])
+
+            # grid_offset --> (h, w, n, 2)
+            grid_offset = grid + initial_offsets
+
+            input_deform = self._tf_batch_map_offsets(self.inputs, offset_layer, grid_offset)
 
             W = self._get_tf_variable(
-                name='W_deformableconv2d', shape=[1, 1, shape[0] * shape[1], shape[-2], shape[-1]], initializer=W_init,
-                dtype=self.inputs.dtype, **self.W_init_args
+                name='W_deformableconv2d', shape=(1, 1, w_shape[0] * w_shape[1], w_shape[-2], w_shape[-1]),
+                initializer=self.W_init, dtype=self.inputs.dtype, **self.W_init_args
             )
 
             _tensor = tf.nn.conv3d(input_deform, W, strides=[1, 1, 1, 1, 1], padding='VALID', name=None)
 
-            if b_init:
+            if self.b_init:
                 b = self._get_tf_variable(
-                    name='b_deformableconv2d', shape=(shape[-1]), initializer=b_init, dtype=self.inputs.dtype,
+                    name='b_deformableconv2d', shape=(w_shape[-1]), initializer=self.b_init, dtype=self.inputs.dtype,
                     **self.b_init_args
                 )
 
                 _tensor = tf.nn.bias_add(_tensor, b, name='bias_add')
 
             self.outputs = tf.reshape(
-                tensor=self._apply_activation(_tensor), shape=[tf.shape(self.inputs)[0], input_h, input_w, shape[-1]]
+                tensor=self._apply_activation(_tensor), shape=[tf.shape(self.inputs)[0], input_h, input_w, w_shape[-1]]
             )
 
         self._add_layers(self.outputs)
-
-        if b_init:
-            self._add_params([W, b])
-        else:
-            self._add_params(W)
+        self._add_params(self._local_weights)
 
     @private_method
     def _to_bc_h_w(self, x, x_shape):
