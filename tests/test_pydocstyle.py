f5f43826cdeb07b287bea5160f30dcd01247e33a
lg <lg4869@outlook.com>
2018-08-14 20:23:14 +0800

    cleanup docs (fix D208)

--- tests/test_pydocstyle.py
+++ tests/test_pydocstyle.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env pytest
+
 
 import os
 import unittest
@@ -31,34 +31,54 @@ class PyDOC_Style_Test(unittest.TestCase):
 
         cls.violations = list()
 
+        # TODO: fix all violations to make it empty
         _disabled_checks = [
             'D202',  # No blank lines allowed after function docstring
             'D205',  # 1 blank line required between summary line and description
+            'D102',  # Missing docstring in public method
+            'D400',  # First line should end with a period
+            'D205',  # 1 blank line required between summary line and description
+            'D100',  # Missing docstring in public module
+            'D107',  # Missing docstring in __init__
+            'D103',  # Missing docstring in public function
+            'D401',  # First line should be in imperative mood
+            'D101',  # Missing docstring in public class
+            'D413',  # Missing blank line after last section
+            'D202',  # No blank lines allowed after function docstring
+            'D210',  # No whitespaces allowed surrounding docstring text
+            'D200',  # One-line docstring should fit on one line with quotes
+            'D105',  # Missing docstring in magic method
+            'D301',  # Use r""" if any backslashes in a docstring
+            'D207',  # Docstring is under-indented
+            'D403',  # First word of the first line should be properly capitalized
+            'D204',  # 1 blank line required after class docstring
+            'D104',  # Missing docstring in public package
+            'D412',  # No blank lines allowed between a section header and its content
+            'D402',  # First line should not be the function's "signature"
+            'D300',  # Use """triple double quotes"""
+            # 'D208',  # Docstring is over-indented
         ]
 
         for filename in testing.list_all_py_files():
-
             print(filename)
             for err in check([filename]):
-
                 if not err.code in _disabled_checks:
                     cls.violations.append(err)
 
     def test_violations(self):
         if self.violations:
-
             counts = dict()
 
             for err in self.violations:
                 counts[err.code] = counts.get(err.code, 0) + 1
+                print(err)
 
             for n, code in sorted([(n, code) for code, n in counts.items()], reverse=True):
                 p = lookup_error_params(code)
                 print('%s %8d %s' % (code, n, p.short_desc))
 
-            print()
-
-            #raise Exception('PyDoc Coding Style: %d violations have been found' % ( len(self.violations)))  ## TODO: Correct these errors to allow Exception
+            raise Exception('PyDoc Coding Style: %d violations have been found' %
+                            (len(self.violations)))
 
 
 if __name__ == '__main__':
