5e41a5a512354cbde29790e32fd76b3a36ee9d23
JUNBIN WANG <junbinwang@dyn3202-50.wlan.ic.ac.uk>
2018-08-23 19:06:16 +0100

    fix format issue

--- tensorlayer/layers/image_resampling.py
+++ tensorlayer/layers/image_resampling.py
@@ -16,8 +16,7 @@ __all__ = [
 
 
 class UpSampling2dLayer(Layer):
-    """The :class:`UpSampling2dLayer` class is a up-sampling 2D layer.
-    See `tf.image.resize_images <https://www.tensorflow.org/api_docs/python/tf/image/resize_images>`__.
+    """The :class:`UpSampling2dLayer` class is a up-sampling 2D layer, see `tf.image.resize_images <https://www.tensorflow.org/api_docs/python/tf/image/resize_images>`__.
     Parameters
     ----------
     prev_layer : :class:`Layer`
@@ -87,8 +86,7 @@ class UpSampling2dLayer(Layer):
 
 
 class DownSampling2dLayer(Layer):
-    """The :class:`DownSampling2dLayer` class is down-sampling 2D layer.
-    See `tf.image.resize_images <https://www.tensorflow.org/versions/master/api_docs/python/image/resizing#resize_images>`__.
+    """The :class:`DownSampling2dLayer` class is down-sampling 2D layer, see `tf.image.resize_images <https://www.tensorflow.org/versions/master/api_docs/python/image/resizing#resize_images>`__.
     Parameters
     ----------
     prev_layer : :class:`Layer`
