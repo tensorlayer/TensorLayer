adbbf282f42a2f77435b909c3dfca9b397b35d45
lg <lg4869@outlook.com>
2018-08-25 02:42:54 +0800

    fix docstyle

--- example/basic_tutorials/tutorial_cifar10_datasetapi.py
+++ example/basic_tutorials/tutorial_cifar10_datasetapi.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
-""" Using Dataset API and tf.image can obtain the best performance. """
+"""Using Dataset API and tf.image can obtain the best performance."""
 
 import time
 import multiprocessing
@@ -101,7 +101,7 @@ def _map_fn_test(img, target):
 
 # dataset API and augmentation
 ds = tf.data.Dataset().from_generator(generator_train, output_types=(tf.float32,
-                                                                     tf.int32))  #, output_shapes=((24, 24, 3), (1)))
+                                                                     tf.int32))  # , output_shapes=((24, 24, 3), (1)))
 ds = ds.map(_map_fn_train, num_parallel_calls=multiprocessing.cpu_count())
 ds = ds.repeat(n_epoch)
 ds = ds.shuffle(shuffle_buffer_size)
@@ -110,7 +110,7 @@ ds = ds.batch(batch_size)
 value = ds.make_one_shot_iterator().get_next()
 
 ds = tf.data.Dataset().from_generator(generator_test, output_types=(tf.float32,
-                                                                    tf.int32))  #, output_shapes=((24, 24, 3), (1)))
+                                                                    tf.int32))  # , output_shapes=((24, 24, 3), (1)))
 ds = ds.map(_map_fn_test, num_parallel_calls=multiprocessing.cpu_count())
 ds = ds.repeat(n_epoch)
 ds = ds.shuffle(shuffle_buffer_size)
