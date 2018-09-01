ec33758834c93d128f2eef9a565d169b3d940515
Hao <dhsig552@163.com>
2018-09-01 20:58:05 +0100

    [pls merge] docs and release (#814)
    * docs and release
    
    * Update package_info.py
    
    * Update layers.rst

--- tensorlayer/package_info.py
+++ tensorlayer/package_info.py
@@ -3,7 +3,7 @@
 """Deep learning and Reinforcement learning library for Researchers and Engineers"""
 
 # Use the following formatting: (major, minor, patch, prerelease)
-VERSION = (1, 10, 0, "rc0")
+VERSION = (1, 10, 0, "")
 __shortversion__ = '.'.join(map(str, VERSION[:3]))
 __version__ = '.'.join(map(str, VERSION[:3])) + "".join(VERSION[3:])
 
