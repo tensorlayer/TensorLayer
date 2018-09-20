39fec6b2cd86e928876e539e7a3980acbdf50c13
Jonathan DEKHTIAR <contact@jonathandekhtiar.eu>
2018-09-20 22:20:06 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -164,6 +164,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
 
         # Additionnal Settings
         'Environment :: Console',
