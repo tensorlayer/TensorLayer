f51a2551e5eb7adc2421bde11430c27261b3af18
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2018-10-01 18:58:40 +0300

    Version Number Updated

--- tensorlayer/package_info.py
+++ tensorlayer/package_info.py
@@ -4,7 +4,7 @@
 """Deep learning and Reinforcement learning library for Researchers and Engineers"""
 
 # Use the following formatting: (major, minor, patch, prerelease)
-VERSION = (1, 10, 1, "")
+VERSION = (2, 0, 0, "a0")
 __shortversion__ = '.'.join(map(str, VERSION[:3]))
 __version__ = '.'.join(map(str, VERSION[:3])) + "".join(VERSION[3:])
 
