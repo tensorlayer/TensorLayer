18f18c21d0259135d05be0003dbc7820f2bcbdf2
Hao <dhsig552@163.com>
2018-11-02 19:47:21 +0800

    cv2 transform support 2 modes;

--- tensorlayer/prepro.py
+++ tensorlayer/prepro.py
@@ -551,7 +551,7 @@ def affine_transform(x, transform_matrix, channel_index=2, fill_mode='nearest', 
 apply_transform = affine_transform
 
 
-def affine_transform_cv2(x, transform_matrix, flags=None, borderMode=None):
+def affine_transform_cv2(x, transform_matrix, flags=None, border_mode='constant'):
     """Return transformed images by given an affine matrix in OpenCV format (x is width). (Powered by OpenCV2, faster than ``tl.prepro.affine_transform``)
 
     Parameters
@@ -560,6 +560,9 @@ def affine_transform_cv2(x, transform_matrix, flags=None, borderMode=None):
         An image with dimension of [row, col, channel] (default).
     transform_matrix : numpy.array
         A transform matrix, OpenCV format.
+    border_mode : str
+        - `constant`, pad the image with a constant value (i.e. black or 0)
+        - `replicate`, the row or column at the very edge of the original is replicated to the extra border.
 
     Examples
     --------
@@ -571,10 +574,14 @@ def affine_transform_cv2(x, transform_matrix, flags=None, borderMode=None):
     rows, cols = x.shape[0], x.shape[1]
     if flags is None:
         flags = cv2.INTER_AREA
-    if borderMode is None:
-        borderMode = cv2.BORDER_CONSTANT
+    if border_mode is 'constant':
+        border_mode = cv2.BORDER_CONSTANT
+    elif border_mode is 'replicate':
+        border_mode = cv2.BORDER_REPLICATE
+    else:
+        raise Exception("unsupport border_mode, check cv.BORDER_ for more details.")
     return cv2.warpAffine(x, transform_matrix[0:2,:], \
-            (cols,rows), flags=flags, borderMode=borderMode)
+            (cols,rows), flags=flags, borderMode=border_mode)
 
 
 def affine_transform_keypoints(coords_list, transform_matrix):
