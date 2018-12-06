ab508833115b342898671659090631b71b883262
Hao <dhsig552@163.com>
2018-12-06 15:48:55 +0800

    QuanDense

--- tensorlayer/layers/dense/quan_dense.py
+++ tensorlayer/layers/dense/quan_dense.py
@@ -24,8 +24,6 @@ class QuanDense(Layer):
 
     Parameters
     ----------
-    prev_layer : :class:`Layer`
-        Previous layer.
     n_units : int
         The number of units of this layer.
     act : activation function
@@ -44,15 +42,13 @@ class QuanDense(Layer):
         The arguments for the weight matrix initializer.
     b_init_args : dictionary
         The arguments for the bias vector initializer.
-    name : a str
+    name : None or str
         A unique layer name.
 
     """
 
-    @deprecated_alias(layer='prev_layer', end_support_version=1.9)  # TODO remove this line for the 1.9 release
     def __init__(
             self,
-            prev_layer,
             n_units=100,
             act=None,
             bitW=8,
@@ -62,51 +58,58 @@ class QuanDense(Layer):
             b_init=tf.constant_initializer(value=0.0),
             W_init_args=None,
             b_init_args=None,
-            name='quan_dense',
+            name=None, #'quan_dense',
     ):
-        super(QuanDense, self
-             ).__init__(prev_layer=prev_layer, act=act, W_init_args=W_init_args, b_init_args=b_init_args, name=name)
-
+        # super(QuanDense, self
+        #      ).__init__(prev_layer=prev_layer, act=act, W_init_args=W_init_args, b_init_args=b_init_args, name=name)
+        super().__init__(name)
+        self.n_units=n_units
+        self.act=act
+        self.bitW=bitW
+        self.bitA=bitA
+        self.use_gemm=use_gemm
+        self.W_init=W_init
+        self.b_init=b_init
+        self.W_init_args=W_init_args
+        self.b_init_args=b_init_args
         logging.info(
             "QuanDense  %s: %d %s" %
             (self.name, n_units, self.act.__name__ if self.act is not None else 'No Activation')
         )
-
-        if self.inputs.get_shape().ndims != 2:
+    def build(self, inputs):
+        if inputs.get_shape().ndims != 2:
             raise Exception("The input dimension must be rank 2, please reshape or flatten it")
 
-        if use_gemm:
+        if self.use_gemm:
             raise Exception("TODO. The current version use tf.matmul for inferencing.")
 
-        n_in = int(self.inputs.get_shape()[-1])
-        self.inputs = quantize_active_overflow(self.inputs, bitA)
-        self.n_units = n_units
+        n_in = int(inputs.get_shape()[-1])
 
-        with tf.variable_scope(name):
-
-            W = tf.get_variable(
-                name='W', shape=(n_in, n_units), initializer=W_init, dtype=LayersConfig.tf_dtype, **self.W_init_args
+        self.W = tf.get_variable(
+                name=self.name+'\W', shape=(n_in, self.n_units), initializer=self.W_init, dtype=LayersConfig.tf_dtype, **self.W_init_args
             )
 
-            W = quantize_weight_overflow(W, bitW)
-
-            self.outputs = tf.matmul(self.inputs, W)
-
-            if b_init is not None:
-                try:
-                    b = tf.get_variable(
-                        name='b', shape=(n_units), initializer=b_init, dtype=LayersConfig.tf_dtype, **self.b_init_args
-                    )
-                except Exception:  # If initializer is a constant, do not specify shape.
-                    b = tf.get_variable(name='b', initializer=b_init, dtype=LayersConfig.tf_dtype, **self.b_init_args)
-
-                self.outputs = tf.nn.bias_add(self.outputs, b, name='bias_add')
-
-            self.outputs = self._apply_activation(self.outputs)
-
-        self._add_layers(self.outputs)
-
-        if b_init is not None:
-            self._add_params([W, b])
+        if self.b_init is not None:
+            try:
+                self.b = tf.get_variable(
+                    name=self.name+'\b', shape=(self.n_units), initializer=self.b_init, dtype=LayersConfig.tf_dtype, **self.b_init_args
+                )
+            except Exception:  # If initializer is a constant, do not specify shape.
+                self.b = tf.get_variable(name=self.name+'\b', initializer=self.b_init, dtype=LayersConfig.tf_dtype, **self.b_init_args)
+            self.add_weights([self.W, self.b])
         else:
-            self._add_params(W)
+            self.add_weights(self.W)
+
+    def forward(self, inputs):
+
+        inputs = quantize_active_overflow(inputs, self.bitA)
+
+        W_ = quantize_weight_overflow(self.W, self.bitW)
+
+        outputs = tf.matmul(inputs, self.W)
+
+        if self.b_init is not None:
+            outputs = tf.nn.bias_add(outputs, self.b, name='bias_add')
+        if self.act:
+            outputs = self.act(outputs)
+        return outputs
