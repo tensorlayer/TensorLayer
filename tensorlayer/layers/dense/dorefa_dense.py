587dcb444a40d22b4c46da303eb793d0c47d1822
Hao <dhsig552@163.com>
2018-12-06 16:54:23 +0800

    DorefaDense

--- tensorlayer/layers/dense/dorefa_dense.py
+++ tensorlayer/layers/dense/dorefa_dense.py
@@ -27,8 +27,6 @@ class DorefaDense(Layer):
 
     Parameters
     ----------
-    prev_layer : :class:`Layer`
-        Previous layer.
     bitW : int
         The bits of this layer's parameter
     bitA : int
@@ -52,10 +50,8 @@ class DorefaDense(Layer):
 
     """
 
-    @deprecated_alias(layer='prev_layer', end_support_version=1.9)  # TODO remove this line for the 1.9 release
     def __init__(
             self,
-            prev_layer,
             bitW=1,
             bitA=3,
             n_units=100,
@@ -65,55 +61,55 @@ class DorefaDense(Layer):
             b_init=tf.constant_initializer(value=0.0),
             W_init_args=None,
             b_init_args=None,
-            name='dorefa_dense',
+            name=None, #'dorefa_dense',
     ):
-        super(DorefaDense, self
-             ).__init__(prev_layer=prev_layer, act=act, W_init_args=W_init_args, b_init_args=b_init_args, name=name)
-
+        # super(DorefaDense, self
+        #      ).__init__(prev_layer=prev_layer, act=act, W_init_args=W_init_args, b_init_args=b_init_args, name=name)
+        super().__init__(name)
+        self.bitW=bitW
+        self.bitA=bitA
+        self.n_units=n_units
+        self.act=act
+        self.use_gemm=use_gemm
+        self.W_init=W_init
+        self.b_init=b_init
+        self.W_init_args=W_init_args
+        self.b_init_args=b_init_args
         logging.info(
             "DorefaDense  %s: %d %s" %
             (self.name, n_units, self.act.__name__ if self.act is not None else 'No Activation')
         )
 
-        if self.inputs.get_shape().ndims != 2:
+    def build(self, inputs):
+        if inputs.get_shape().ndims != 2:
             raise Exception("The input dimension must be rank 2, please reshape or flatten it")
-        if use_gemm:
+        if self.use_gemm:
             raise Exception("TODO. The current version use tf.matmul for inferencing.")
 
-        n_in = int(self.inputs.get_shape()[-1])
-        self.n_units = n_units
+        n_in = int(inputs.get_shape()[-1])
+        self.W = tf.get_variable(
+            name=self.name+'\W', shape=(n_in, self.n_units), initializer=self.W_init, dtype=LayersConfig.tf_dtype, **self.W_init_args
+        )
+        if self.b_init is not None:
+            try:
+                self.b = tf.get_variable(
+                    name=self.name+'\b', shape=(self.n_units), initializer=self.b_init, dtype=LayersConfig.tf_dtype, **self.b_init_args
+                )
 
-        self.inputs = quantize_active(cabs(self.inputs), bitA)
-
-        with tf.variable_scope(name):
-
-            W = tf.get_variable(
-                name='W', shape=(n_in, n_units), initializer=W_init, dtype=LayersConfig.tf_dtype, **self.W_init_args
-            )
-            # W = tl.act.sign(W)    # dont update ...
-            W = quantize_weight(W, bitW)
-            # W = tf.Variable(W)
-            # print(W)
-
-            self.outputs = tf.matmul(self.inputs, W)
-            # self.outputs = xnor_gemm(self.inputs, W) # TODO
-
-            if b_init is not None:
-                try:
-                    b = tf.get_variable(
-                        name='b', shape=(n_units), initializer=b_init, dtype=LayersConfig.tf_dtype, **self.b_init_args
-                    )
-
-                except Exception:  # If initializer is a constant, do not specify shape.
-                    b = tf.get_variable(name='b', initializer=b_init, dtype=LayersConfig.tf_dtype, **self.b_init_args)
-
-                self.outputs = tf.nn.bias_add(self.outputs, b, name='bias_add')
-                # self.outputs = xnor_gemm(self.inputs, W) + b # TODO
-
-            self.outputs = self._apply_activation(self.outputs)
-
-        self._add_layers(self.outputs)
-        if b_init is not None:
-            self._add_params([W, b])
+            except Exception:  # If initializer is a constant, do not specify shape.
+                self.b = tf.get_variable(name=self.name+'\b', initializer=self.b_init, dtype=LayersConfig.tf_dtype, **self.b_init_args)
+            self.add_weights([self.W, self.b])
         else:
-            self._add_params(W)
+            self.add_weights(self.W)
+
+    def forward(self, inputs):
+        inputs = quantize_active(cabs(inputs), self.bitA)
+        W_ = quantize_weight(self.W, self.bitW)
+        outputs = tf.matmul(inputs, W_)
+        # self.outputs = xnor_gemm(self.inputs, W) # TODO
+        if self.b_init is not None:
+            outputs = tf.nn.bias_add(outputs, self.b, name='bias_add')
+            # self.outputs = xnor_gemm(self.inputs, W) + b # TODO
+        if self.act:
+            outputs = self.act(outputs)
+        return outputs
