e53a772fb78daa55a983ca18c410f14aee0f2f20
Hao <dhsig552@163.com>
2018-12-06 16:28:49 +0800

    GroupConv2d

--- tensorlayer/layers/convolution/group_conv.py
+++ tensorlayer/layers/convolution/group_conv.py
@@ -20,8 +20,6 @@ class GroupConv2d(Layer):
 
     Parameters
     --------------
-    prev_layer : :class:`Layer`
-        Previous layer.
     n_filter : int
         The number of filters.
     filter_size : int
@@ -42,14 +40,12 @@ class GroupConv2d(Layer):
         The arguments for the weight matrix initializer.
     b_init_args : dictionary
         The arguments for the bias vector initializer.
-    name : str
+    name : None or str
         A unique layer name.
     """
 
-    @deprecated_alias(layer='prev_layer', end_support_version=1.9)  # TODO remove this line for the 1.9 release
     def __init__(
             self,
-            prev_layer,
             n_filter=32,
             filter_size=(3, 3),
             strides=(2, 2),
@@ -58,53 +54,56 @@ class GroupConv2d(Layer):
             padding='SAME',
             W_init=tf.truncated_normal_initializer(stddev=0.02),
             b_init=tf.constant_initializer(value=0.0),
-            W_init_args=None,  # TODO: Remove when TF <1.3 not supported
-            b_init_args=None,  # TODO: Remove when TF <1.3 not supported
-            name='groupconv',
+            W_init_args=None,
+            b_init_args=None,
+            name=None, #'groupconv',
     ):  # Windaway
 
-        super(GroupConv2d, self
-             ).__init__(prev_layer=prev_layer, act=act, W_init_args=W_init_args, b_init_args=b_init_args, name=name)
-
+        # super(GroupConv2d, self
+        #      ).__init__(prev_layer=prev_layer, act=act, W_init_args=W_init_args, b_init_args=b_init_args, name=name)
+        super().__init__(name)
+        self.n_filter=n_filter
+        self.filter_size=filter_size
+        self.strides=strides
+        self.n_group=n_group
+        self.act=act
+        self.padding=padding
+        self.W_init=W_init
+        self.b_init=b_init
+        self.W_init_args=W_init_args
+        self.b_init_args=b_init_args
         logging.info(
             "GroupConv2d %s: n_filter: %d size: %s strides: %s n_group: %d pad: %s act: %s" % (
                 self.name, n_filter, str(filter_size), str(strides), n_group, padding,
                 self.act.__name__ if self.act is not None else 'No Activation'
             )
         )
+    def build(self, inputs):
+        self.groupConv = lambda i, k: tf.nn.conv2d(i, k, strides=[1, self.strides[0], self.strides[1], 1], padding=self.padding)
+        channels = int(inputs.get_shape()[-1])
 
-        groupConv = lambda i, k: tf.nn.conv2d(i, k, strides=[1, strides[0], strides[1], 1], padding=padding)
-        channels = int(self.inputs.get_shape()[-1])
-
-        with tf.variable_scope(name):
-            We = tf.get_variable(
-                name='W', shape=[filter_size[0], filter_size[1], channels / n_group, n_filter], initializer=W_init,
-                dtype=LayersConfig.tf_dtype, trainable=True, **self.W_init_args
+        self.We = tf.get_variable(
+            name=self.name+'\W', shape=[self.filter_size[0], self.filter_size[1], channels / self.n_group, self.n_filter], initializer=self.W_init,
+            dtype=LayersConfig.tf_dtype, trainable=True, **self.W_init_args
+        )
+        if self.b_init:
+            self.b = tf.get_variable(
+                name=self.name+'\b', shape=self.n_filter, initializer=self.b_init, dtype=LayersConfig.tf_dtype, trainable=True,
+                **self.b_init_args
             )
-
-            if n_group == 1:
-                self.outputs = groupConv(self.inputs, We)
-
-            else:
-                inputGroups = tf.split(axis=3, num_or_size_splits=n_group, value=self.inputs)
-                weightsGroups = tf.split(axis=3, num_or_size_splits=n_group, value=We)
-                convGroups = [groupConv(i, k) for i, k in zip(inputGroups, weightsGroups)]
-
-                self.outputs = tf.concat(axis=3, values=convGroups)
-
-            if b_init:
-                b = tf.get_variable(
-                    name='b', shape=n_filter, initializer=b_init, dtype=LayersConfig.tf_dtype, trainable=True,
-                    **self.b_init_args
-                )
-
-                self.outputs = tf.nn.bias_add(self.outputs, b, name='bias_add')
-
-            self.outputs = self._apply_activation(self.outputs)
-
-        self._add_layers(self.outputs)
-
-        if b_init:
-            self._add_params([We, b])
+            self.add_weights([self.We, self.b])
         else:
-            self._add_params(We)
+            self.add_weights(self.We)
+    def forward(self, inputs):
+        if self.n_group == 1:
+            outputs = self.groupConv(inputs, self.We)
+        else:
+            inputGroups = tf.split(axis=3, num_or_size_splits=self.n_group, value=self.inputs)
+            weightsGroups = tf.split(axis=3, num_or_size_splits=self.n_group, value=self.We)
+            convGroups = [groupConv(i, k) for i, k in zip(inputGroups, weightsGroups)]
+            outputs = tf.concat(axis=3, values=convGroups)
+        if self.b_init:
+            outputs = tf.nn.bias_add(outputs, self.b, name='bias_add')
+        if self.act:
+            outputs = self.act(outputs)
+        return outputs
