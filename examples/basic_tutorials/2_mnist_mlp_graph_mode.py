84fa7def0fdc0b8e624c5968d4a48b44fd1b984b
Hao <dhsig552@163.com>
2019-01-24 13:14:12 +0800

    examples / support is_train and switching method

--- examples/basic_tutorials/2_mnist_mlp_graph_mode.py
+++ examples/basic_tutorials/2_mnist_mlp_graph_mode.py
@@ -44,8 +44,15 @@ MLP = get_model([None, 784])
 x = tf.placeholder(tf.float32, shape=[None, 784], name='inputs')
 y_ = tf.placeholder(tf.int64, shape=[None], name='targets')
 
+## get output tensors for training and testing
+# 1) use ``is_train''
 y1 = MLP(x, is_train=True)
 y2 = MLP(x, is_train=False)
+# 2) alternatively, you can use the switching method
+# MLP.train()
+# y1 = MLP(x)
+# MLP.eval()
+# y2 = MLP(x)
 
 ## cost and optimizer for training
 cost = tl.cost.cross_entropy(y1, y_, name='train_loss')
