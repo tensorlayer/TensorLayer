0686d1c03bc38a4812c51670ef72a84573e23e85
1FengL <ivbensekin@gmail.com>
2019-04-06 01:13:31 +0100

    add layer name uniqueness test

--- tests/models/test_auto_naming.py
+++ tests/models/test_auto_naming.py
@@ -14,12 +14,12 @@ from tensorlayer.models import *
 from tests.utils import CustomTestCase
 
 
-def basic_static_model(name=None):
+def basic_static_model(name=None, conv1_name="conv1", conv2_name="conv2"):
     ni = Input((None, 24, 24, 3))
-    nn = Conv2d(16, (5, 5), (1, 1), padding='SAME', act=tf.nn.relu, name="conv1")(ni)
+    nn = Conv2d(16, (5, 5), (1, 1), padding='SAME', act=tf.nn.relu, name=conv1_name)(ni)
     nn = MaxPool2d((3, 3), (2, 2), padding='SAME', name='pool1')(nn)
 
-    nn = Conv2d(16, (5, 5), (1, 1), padding='SAME', act=tf.nn.relu, name="conv2")(nn)
+    nn = Conv2d(16, (5, 5), (1, 1), padding='SAME', act=tf.nn.relu, name=conv2_name)(nn)
     nn = MaxPool2d((3, 3), (2, 2), padding='SAME', name='pool2')(nn)
 
     M = Model(inputs=ni, outputs=nn, name=name)
@@ -34,12 +34,12 @@ def nested_static_model(name=None, inner_model_name=None):
 
 
 class basic_dynamic_model(Model):
-    def __init__(self, name=None):
+    def __init__(self, name=None, conv1_name="conv1", conv2_name="conv2"):
         super(basic_dynamic_model, self).__init__(name=name)
-        self.conv1 = Conv2d(16, (5, 5), (1, 1), padding='SAME', act=tf.nn.relu, in_channels=3, name="conv1")
+        self.conv1 = Conv2d(16, (5, 5), (1, 1), padding='SAME', act=tf.nn.relu, in_channels=3, name=conv1_name)
         self.pool1 = MaxPool2d((3, 3), (2, 2), padding='SAME', name='pool1')
 
-        self.conv2 = Conv2d(16, (5, 5), (1, 1), padding='SAME', act=tf.nn.relu, in_channels=16, name="conv2")
+        self.conv2 = Conv2d(16, (5, 5), (1, 1), padding='SAME', act=tf.nn.relu, in_channels=16, name=conv2_name)
         self.pool2 = MaxPool2d((3, 3), (2, 2), padding='SAME', name='pool2')
 
     def forward(self, x):
@@ -47,9 +47,6 @@ class basic_dynamic_model(Model):
         x = self.pool1(x)
         x = self.conv2(x)
         x = self.pool2(x)
-        x = self.flatten(x)
-        x = self.dense1(x)
-        x = self.dense2(x)
         return x
 
 
@@ -149,6 +146,22 @@ class Auto_Naming_Test(CustomTestCase):
         try:
             model_nested_given_repeat_name = nested_static_model(inner_model_name="a_inner_static_model")
             self.fail("Failed to detect nested repeat user given names")
+        except Exception as e:
+            pass
+
+    def test_layer_name_uniqueness(self):
+        print('-' * 20, 'test_layer_name_uniqueness', '-' * 20)
+        # dynamic
+        try:
+            model_dynamic = basic_dynamic_model(conv1_name="conv", conv2_name="conv")
+            self.fail("Failed to detect that layers inside a model have the same name in dynamic mode")
+        except Exception as e:
+            pass
+        
+        # static
+        try:
+            model_static = basic_static_model(conv1_name="conv", conv2_name="conv")
+            self.fail("Failed to detect that layers inside a model have the same name in static mode")
         except Exception as e:
             pass
 
