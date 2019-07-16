c064ee09cb8730339f6c33057c123e460b710359
RuihaiWu <37874862+warshallrho@users.noreply.github.com>
2019-07-17 01:23:23 +0800

    yapf

--- tensorlayer/models/vgg.py
+++ tensorlayer/models/vgg.py
@@ -49,9 +49,9 @@ __all__ = [
 ]
 
 layer_names = [
-    ['conv1_1', 'conv1_2'], 'pool1', ['conv2_1', 'conv2_2'], 'pool2', ['conv3_1', 'conv3_2', 'conv3_3', 'conv3_4'],
-    'pool3', ['conv4_1', 'conv4_2', 'conv4_3', 'conv4_4'], 'pool4', ['conv5_1', 'conv5_2', 'conv5_3', 'conv5_4'],
-    'pool5', 'flatten', 'fc1_relu', 'fc2_relu', 'outputs'
+    ['conv1_1', 'conv1_2'], 'pool1', ['conv2_1', 'conv2_2'], 'pool2',
+    ['conv3_1', 'conv3_2', 'conv3_3', 'conv3_4'], 'pool3', ['conv4_1', 'conv4_2', 'conv4_3', 'conv4_4'], 'pool4',
+    ['conv5_1', 'conv5_2', 'conv5_3', 'conv5_4'], 'pool5', 'flatten', 'fc1_relu', 'fc2_relu', 'outputs'
 ]
 
 cfg = {
