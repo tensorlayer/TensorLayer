09cda3758540876e94ec651762fedbfcde91aba1
Rundi Wu <rundi_wu@pku.edu.cn>
2019-07-25 21:41:48 -0400

    resnet50 (static) finished and tested with example

--- tensorlayer/models/__init__.py
+++ tensorlayer/models/__init__.py
@@ -4,6 +4,7 @@
 # """A collections of pre-defined well known models."""
 
 from .core import *
+from .resnet import ResNet50
 from .mobilenetv1 import MobileNetV1
 from .squeezenetv1 import SqueezeNetV1
 from .vgg import *
