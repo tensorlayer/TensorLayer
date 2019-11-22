24f7b1eb8883ae5f947b26e7fa65dc4c181aa9c2
Hao <dhsig552@163.com>
2019-11-22 10:52:51 +0800

    Update image_resampling.py

--- tensorlayer/layers/image_resampling.py
+++ tensorlayer/layers/image_resampling.py
@@ -89,7 +89,7 @@ class UpSampling2d(Layer):
         inputs : :class:`Tensor`
             Inputs tensors with 4-D Tensor of the shape (batch, height, width, channels)
         """
-        output_size = [inputs.shape[1] * self.scale[0], inputs.shape[2] * self.scale[1]]
+        output_size = [int(inputs.shape[1] * self.scale[0]), int(inputs.shape[2] * self.scale[1])]
         outputs = tf.image.resize(inputs, size=output_size, method=self.method, antialias=self.antialias)
         return outputs
 
