ad8b2b9d08eec5271581dabdf75aec3a83190f0b
One <1137743903@qq.com>
2020-06-18 11:16:56 +0800

    hyperpose compatible: (1)maxpool and batchnorm dataformat debuged,support "channels_first" (2)vgg forward fixed

--- tensorlayer/layers/pooling.py
+++ tensorlayer/layers/pooling.py
@@ -31,7 +31,6 @@ class PoolLayer(Layer):
     The :class:`PoolLayer` class is a Pooling layer.
     You can choose ``tf.nn.max_pool`` and ``tf.nn.avg_pool`` for 2D input or
     ``tf.nn.max_pool3d`` and ``tf.nn.avg_pool3d`` for 3D input.
-
     Parameters
     ----------
     filter_size : tuple of int
@@ -47,15 +46,12 @@ class PoolLayer(Layer):
         See `TensorFlow pooling APIs <https://tensorflow.google.cn/versions/r2.0/api_docs/python/tf/nn/>`__
     name : None or str
         A unique layer name.
-
     Examples
     ---------
     With TensorLayer
-
     >>> net = tl.layers.Input([None, 50, 50, 32], name='input')
     >>> net = tl.layers.PoolLayer()(net)
     >>> output shape : [None, 25, 25, 32]
-
     """
 
     def __init__(
@@ -97,7 +93,6 @@ class PoolLayer(Layer):
 
 class MaxPool1d(Layer):
     """Max pooling for 1D signal.
-
     Parameters
     ----------
     filter_size : int
@@ -110,15 +105,12 @@ class MaxPool1d(Layer):
         One of channels_last (default, [batch, length, channel]) or channels_first. The ordering of the dimensions in the inputs.
     name : None or str
         A unique layer name.
-
     Examples
     ---------
     With TensorLayer
-
     >>> net = tl.layers.Input([None, 50, 32], name='input')
     >>> net = tl.layers.MaxPool1d(filter_size=3, strides=2, padding='SAME', name='maxpool1d')(net)
     >>> output shape : [None, 25, 32]
-
     """
 
     def __init__(
@@ -182,7 +174,6 @@ class MaxPool1d(Layer):
 
 class MeanPool1d(Layer):
     """Mean pooling for 1D signal.
-
     Parameters
     ------------
     filter_size : int
@@ -195,15 +186,12 @@ class MeanPool1d(Layer):
         One of channels_last (default, [batch, length, channel]) or channels_first. The ordering of the dimensions in the inputs.
     name : None or str
         A unique layer name.
-
     Examples
     ---------
     With TensorLayer
-
     >>> net = tl.layers.Input([None, 50, 32], name='input')
     >>> net = tl.layers.MeanPool1d(filter_size=3, strides=2, padding='SAME')(net)
     >>> output shape : [None, 25, 32]
-
     """
 
     def __init__(
@@ -268,7 +256,6 @@ class MeanPool1d(Layer):
 
 class MaxPool2d(Layer):
     """Max pooling for 2D image.
-
     Parameters
     -----------
     filter_size : tuple of int
@@ -281,15 +268,12 @@ class MaxPool2d(Layer):
         One of channels_last (default, [batch, height, width, channel]) or channels_first. The ordering of the dimensions in the inputs.
     name : None or str
         A unique layer name.
-
     Examples
     ---------
     With TensorLayer
-
     >>> net = tl.layers.Input([None, 50, 50, 32], name='input')
     >>> net = tl.layers.MaxPool2d(filter_size=(3, 3), strides=(2, 2), padding='SAME')(net)
     >>> output shape : [None, 25, 25, 32]
-
     """
 
     def __init__(
@@ -324,24 +308,29 @@ class MaxPool2d(Layer):
         return s.format(classname=self.__class__.__name__, **self.__dict__)
 
     def build(self, inputs_shape=None):
-        self._strides = [1, self.strides[0], self.strides[1], 1]
         if self.data_format == 'channels_last':
+            self._strides = [1, self.strides[0], self.strides[1], 1]
             self.data_format = 'NHWC'
         elif self.data_format == 'channels_first':
             self.data_format = 'NCHW'
+            self._strides = [1, 1, self.strides[0], self.strides[1]]
         else:
             raise Exception("unsupported data format")
 
     def forward(self, inputs):
         outputs = tf.nn.max_pool(
-            input=inputs, ksize=self.filter_size, strides=self._strides, padding=self.padding, name=self.name
+            input=inputs,
+            ksize=self.filter_size,
+            strides=self._strides,
+            padding=self.padding,
+            name=self.name,
+            data_format=self.data_format
         )
         return outputs
 
 
 class MeanPool2d(Layer):
     """Mean pooling for 2D image [batch, height, width, channel].
-
     Parameters
     -----------
     filter_size : tuple of int
@@ -354,15 +343,12 @@ class MeanPool2d(Layer):
         One of channels_last (default, [batch, height, width, channel]) or channels_first. The ordering of the dimensions in the inputs.
     name : None or str
         A unique layer name.
-
     Examples
     ---------
     With TensorLayer
-
     >>> net = tl.layers.Input([None, 50, 50, 32], name='input')
     >>> net = tl.layers.MeanPool2d(filter_size=(3, 3), strides=(2, 2), padding='SAME')(net)
     >>> output shape : [None, 25, 25, 32]
-
     """
 
     def __init__(
@@ -397,24 +383,29 @@ class MeanPool2d(Layer):
         return s.format(classname=self.__class__.__name__, **self.__dict__)
 
     def build(self, inputs_shape=None):
-        self._strides = [1, self.strides[0], self.strides[1], 1]
         if self.data_format == 'channels_last':
             self.data_format = 'NHWC'
+            self._strides = [1, self.strides[0], self.strides[1], 1]
         elif self.data_format == 'channels_first':
             self.data_format = 'NCHW'
+            self._strides = [1, 1, self.strides[0], self.strides[1]]
         else:
             raise Exception("unsupported data format")
 
     def forward(self, inputs):
         outputs = tf.nn.avg_pool(
-            input=inputs, ksize=self.filter_size, strides=self._strides, padding=self.padding, name=self.name
+            input=inputs,
+            ksize=self.filter_size,
+            strides=self._strides,
+            padding=self.padding,
+            name=self.name,
+            data_format=self.data_format
         )
         return outputs
 
 
 class MaxPool3d(Layer):
     """Max pooling for 3D volume.
-
     Parameters
     ------------
     filter_size : tuple of int
@@ -427,20 +418,16 @@ class MaxPool3d(Layer):
         One of channels_last (default, [batch, depth, height, width, channel]) or channels_first. The ordering of the dimensions in the inputs.
     name : None or str
         A unique layer name.
-
     Returns
     -------
     :class:`tf.Tensor`
         A max pooling 3-D layer with a output rank as 5.
-
     Examples
     ---------
     With TensorLayer
-
     >>> net = tl.layers.Input([None, 50, 50, 50, 32], name='input')
     >>> net = tl.layers.MaxPool3d(filter_size=(3, 3, 3), strides=(2, 2, 2), padding='SAME')(net)
     >>> output shape : [None, 25, 25, 25, 32]
-
     """
 
     def __init__(
@@ -473,11 +460,12 @@ class MaxPool3d(Layer):
         return s.format(classname=self.__class__.__name__, **self.__dict__)
 
     def build(self, inputs_shape=None):
-        self._strides = [1, self.strides[0], self.strides[1], self.strides[2], 1]
         if self.data_format == 'channels_last':
             self.data_format = 'NDHWC'
+            self._strides = [1, self.strides[0], self.strides[1], self.strides[2], 1]
         elif self.data_format == 'channels_first':
             self.data_format = 'NCDHW'
+            self._strides = [1, 1, self.strides[0], self.strides[1], self.strides[2]]
         else:
             raise Exception("unsupported data format")
 
@@ -495,7 +483,6 @@ class MaxPool3d(Layer):
 
 class MeanPool3d(Layer):
     """Mean pooling for 3D volume.
-
     Parameters
     ------------
     filter_size : tuple of int
@@ -508,20 +495,16 @@ class MeanPool3d(Layer):
         One of channels_last (default, [batch, depth, height, width, channel]) or channels_first. The ordering of the dimensions in the inputs.
     name : None or str
         A unique layer name.
-
     Returns
     -------
     :class:`tf.Tensor`
         A mean pooling 3-D layer with a output rank as 5.
-
     Examples
     ---------
     With TensorLayer
-
     >>> net = tl.layers.Input([None, 50, 50, 50, 32], name='input')
     >>> net = tl.layers.MeanPool3d(filter_size=(3, 3, 3), strides=(2, 2, 2), padding='SAME')(net)
     >>> output shape : [None, 25, 25, 25, 32]
-
     """
 
     def __init__(
@@ -554,11 +537,12 @@ class MeanPool3d(Layer):
         return s.format(classname=self.__class__.__name__, **self.__dict__)
 
     def build(self, inputs_shape=None):
-        self._strides = [1, self.strides[0], self.strides[1], self.strides[2], 1]
         if self.data_format == 'channels_last':
             self.data_format = 'NDHWC'
+            self._strides = [1, self.strides[0], self.strides[1], self.strides[2], 1]
         elif self.data_format == 'channels_first':
             self.data_format = 'NCDHW'
+            self._strides = [1, 1, self.strides[0], self.strides[1], self.strides[2]]
         else:
             raise Exception("unsupported data format")
 
@@ -576,22 +560,18 @@ class MeanPool3d(Layer):
 
 class GlobalMaxPool1d(Layer):
     """The :class:`GlobalMaxPool1d` class is a 1D Global Max Pooling layer.
-
     Parameters
     ------------
     data_format : str
         One of channels_last (default, [batch, length, channel]) or channels_first. The ordering of the dimensions in the inputs.
     name : None or str
         A unique layer name.
-
     Examples
     ---------
     With TensorLayer
-
     >>> net = tl.layers.Input([None, 100, 30], name='input')
     >>> net = tl.layers.GlobalMaxPool1d()(net)
     >>> output shape : [None, 30]
-
     """
 
     def __init__(
@@ -632,22 +612,18 @@ class GlobalMaxPool1d(Layer):
 
 class GlobalMeanPool1d(Layer):
     """The :class:`GlobalMeanPool1d` class is a 1D Global Mean Pooling layer.
-
     Parameters
     ------------
     data_format : str
         One of channels_last (default, [batch, length, channel]) or channels_first. The ordering of the dimensions in the inputs.
     name : None or str
         A unique layer name.
-
     Examples
     ---------
     With TensorLayer
-
     >>> net = tl.layers.Input([None, 100, 30], name='input')
     >>> net = tl.layers.GlobalMeanPool1d()(net)
     >>> output shape : [None, 30]
-
     """
 
     def __init__(
@@ -687,22 +663,18 @@ class GlobalMeanPool1d(Layer):
 
 class GlobalMaxPool2d(Layer):
     """The :class:`GlobalMaxPool2d` class is a 2D Global Max Pooling layer.
-
     Parameters
     ------------
     data_format : str
         One of channels_last (default, [batch, height, width, channel]) or channels_first. The ordering of the dimensions in the inputs.
     name : None or str
         A unique layer name.
-
     Examples
     ---------
     With TensorLayer
-
     >>> net = tl.layers.Input([None, 100, 100, 30], name='input')
     >>> net = tl.layers.GlobalMaxPool2d()(net)
     >>> output shape : [None, 30]
-
     """
 
     def __init__(
@@ -742,22 +714,18 @@ class GlobalMaxPool2d(Layer):
 
 class GlobalMeanPool2d(Layer):
     """The :class:`GlobalMeanPool2d` class is a 2D Global Mean Pooling layer.
-
     Parameters
     ------------
     data_format : str
         One of channels_last (default, [batch, height, width, channel]) or channels_first. The ordering of the dimensions in the inputs.
     name : None or str
         A unique layer name.
-
     Examples
     ---------
     With TensorLayer
-
     >>> net = tl.layers.Input([None, 100, 100, 30], name='input')
     >>> net = tl.layers.GlobalMeanPool2d()(net)
     >>> output shape : [None, 30]
-
     """
 
     def __init__(
@@ -798,22 +766,18 @@ class GlobalMeanPool2d(Layer):
 
 class GlobalMaxPool3d(Layer):
     """The :class:`GlobalMaxPool3d` class is a 3D Global Max Pooling layer.
-
     Parameters
     ------------
     data_format : str
         One of channels_last (default, [batch, depth, height, width, channel]) or channels_first. The ordering of the dimensions in the inputs.
     name : None or str
         A unique layer name.
-
     Examples
     ---------
     With TensorLayer
-
     >>> net = tl.layers.Input([None, 100, 100, 100, 30], name='input')
     >>> net = tl.layers.GlobalMaxPool3d()(net)
     >>> output shape : [None, 30]
-
     """
 
     def __init__(
@@ -854,22 +818,18 @@ class GlobalMaxPool3d(Layer):
 
 class GlobalMeanPool3d(Layer):
     """The :class:`GlobalMeanPool3d` class is a 3D Global Mean Pooling layer.
-
     Parameters
     ------------
     data_format : str
         One of channels_last (default, [batch, depth, height, width, channel]) or channels_first. The ordering of the dimensions in the inputs.
     name : None or str
         A unique layer name.
-
     Examples
     ---------
     With TensorLayer
-
     >>> net = tl.layers.Input([None, 100, 100, 100, 30], name='input')
     >>> net = tl.layers.GlobalMeanPool3d()(net)
     >>> output shape : [None, 30]
-
     """
 
     def __init__(
@@ -909,7 +869,6 @@ class GlobalMeanPool3d(Layer):
 
 class CornerPool2d(Layer):
     """Corner pooling for 2D image [batch, height, width, channel], see `here <https://arxiv.org/abs/1808.01244>`__.
-
     Parameters
     ----------
     mode : str
@@ -917,15 +876,12 @@ class CornerPool2d(Layer):
         Bottomright for the bottom right corner.
     name : None or str
         A unique layer name.
-
     Examples
     ---------
     With TensorLayer
-
     >>> net = tl.layers.Input([None, 32, 32, 8], name='input')
     >>> net = tl.layers.CornerPool2d(mode='TopLeft',name='cornerpool2d')(net)
     >>> output shape : [None, 32, 32, 8]
-
     """
 
     def __init__(
